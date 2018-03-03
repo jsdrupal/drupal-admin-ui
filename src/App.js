7d5cbf77b4c3a35c26864557f03bb2ad0278951c
Sally Young <sally@justafish.co.uk>
2018-03-03 17:47:47 +0000

    Example of falling back to Drupal admin theme

--- src/App.js
+++ src/App.js
@@ -1,21 +1,64 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import {
+  BrowserRouter as Router,
+  Route,
+  Link
+} from 'react-router-dom'
+
+
+const Home = () => (
+  <div>
+    <h1>Hello, world!</h1>
+  </div>
+);
+
+const Permissions = () => (
+  <div>
+    <h1>Permissions</h1>
+    <p>This will be the permissions page.</p>
+  </div>
+);
+
+const routes = {
+  '/': Home,
+  '/admin/people/permissions': Permissions,
+};
+
+class NoMatch extends Component {
+  componentWillReceiveProps(nextProps) {
+    if (!Object.keys(routes).includes(nextProps.location.pathname)) {
+      window.location = window.location.href;
+    }
+  }
+  render() {
+    return null;
+  }
+};
 
 class App extends Component {
+  componentDidMount() {
+    window.history.replaceState(null, null, '/');
+  }
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <Router>
+        <div>
+          <ul>
+            <li><Link to="/">Home</Link></li>
+            <li><Link to="/admin/people/permissions">Permissions</Link></li>
+            <li><Link to="/admin/appearance">Appearance</Link></li>
+            <li><Link to="/node/add">Content</Link></li>
+          </ul>
+
+          <hr/>
+
+          <Route exact path="/" component={Home}/>
+          <Route path="/admin/people/permissions" component={Permissions}/>
+          <Route component={NoMatch} />
+        </div>
+      </Router>
     );
   }
-}
+};
 
 export default App;
