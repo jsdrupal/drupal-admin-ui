375ffc15d23e10dd05144f6c8c495fba086dc240
Matthew Grill <matt.grill@acquia.com>
2018-03-12 13:05:42 -0700

    Allow the fetch to be canceled if the component unmounts.

--- src/components/05_pages/Permissions/Permissions.js
+++ src/components/05_pages/Permissions/Permissions.js
@@ -1,3 +1,5 @@
+import makeCancelable from 'makecancelable';
+
 import React, { Fragment, Component } from 'react';
 import Loading from '../../Helpers/Loading';
 
@@ -6,6 +8,27 @@ const Permissions = class Permissions extends Component {
     loaded: false,
   };
   componentDidMount() {
+    this.cancelFetch = makeCancelable(
+      Promise.all([
+        fetch(
+          `${process.env.REACT_APP_DRUPAL_BASE_URL}/permissions?_format=json`,
+        ).then(res => res.json()),
+        fetch(
+          `${
+            process.env.REACT_APP_DRUPAL_BASE_URL
+          }/jsonapi/user_role/user_role`,
+        ).then(res => res.json()),
+      ])
+        .then(([permissions, { data: roles }]) =>
+          this.setState({ permissions, roles, loaded: true }),
+        )
+        .catch(err => this.setState({ loaded: false, err })),
+    );
+  }
+  componentWillUnmount() {
+    this.cancelFetch();
+  }
+  fetchData = () =>
     Promise.all([
       fetch(
         `${process.env.REACT_APP_DRUPAL_BASE_URL}/permissions?_format=json`,
@@ -13,12 +36,7 @@ const Permissions = class Permissions extends Component {
       fetch(
         `${process.env.REACT_APP_DRUPAL_BASE_URL}/jsonapi/user_role/user_role`,
       ).then(res => res.json()),
-    ])
-      .then(([permissions, { data: roles }]) =>
-        this.setState({ permissions, roles, loaded: true }),
-      )
-      .catch(err => this.setState({ loaded: false, err }));
-  }
+    ]);
   groupPermissions = permissions =>
     Object.entries(
       Object.keys(permissions)
@@ -30,67 +48,63 @@ const Permissions = class Permissions extends Component {
         }, {}),
     );
   render() {
-    return (
-      <Fragment>
-        {!this.state.loaded ? (
-          <Loading />
-        ) : (
-          <table>
-            <thead>
-              <tr>
-                {[
-                  'PERMISSION',
-                  ...this.state.roles.map(role =>
-                    role.attributes.label.toUpperCase(),
-                  ),
-                ].map(label => <td key={`column-${label}`}>{label}</td>)}
-              </tr>
-            </thead>
-            <tbody>
-              {this.groupPermissions(this.state.permissions).map(
-                ([permissionGroupName, permissions]) =>
-                  permissions.length && (
-                    <Fragment key={`fragment-${permissionGroupName}`}>
-                      <tr key={`permissionGroup-${permissionGroupName}`}>
-                        <td colSpan={this.state.roles.length + 1}>
-                          <b>{permissionGroupName}</b>
-                        </td>
-                      </tr>
-                      {permissions.map(permission => (
-                        <tr
-                          key={`permissionGroup-${permissionGroupName}-${
-                            permission.title
+    return !this.state.loaded ? (
+      <Loading />
+    ) : (
+      <table>
+        <thead>
+          <tr>
+            {[
+              'PERMISSION',
+              ...this.state.roles.map(({ attributes: { label } }) =>
+                label.toUpperCase(),
+              ),
+            ].map(label => <td key={`column-${label}`}>{label}</td>)}
+          </tr>
+        </thead>
+        <tbody>
+          {this.groupPermissions(this.state.permissions).map(
+            ([permissionGroupName, permissions]) =>
+              permissions.length && (
+                <Fragment key={`fragment-${permissionGroupName}`}>
+                  <tr key={`permissionGroup-${permissionGroupName}`}>
+                    <td colSpan={this.state.roles.length + 1}>
+                      <b>{permissionGroupName}</b>
+                    </td>
+                  </tr>
+                  {permissions.map(permission => (
+                    <tr
+                      key={`permissionGroup-${permissionGroupName}-${
+                        permission.title
+                      }`}
+                    >
+                      <td>{permission.title}</td>
+                      {this.state.roles.map(({ attributes }) => (
+                        <td
+                          key={`role-${attributes.id}-permission-${
+                            permission.id
                           }`}
                         >
-                          <td>{permission.title}</td>
-                          {this.state.roles.map(({ attributes }) => (
-                            <td
-                              key={`role-${attributes.id}-permission-${
-                                permission.id
-                              }`}
-                            >
-                              {attributes.is_admin &&
-                              attributes.id === 'administrator' ? (
-                                <input type="checkbox" checked />
-                              ) : (
-                                <input
-                                  type="checkbox"
-                                  checked={attributes.permissions.includes(
-                                    permission.id,
-                                  )}
-                                />
+                          {attributes.is_admin &&
+                          attributes.id === 'administrator' ? (
+                            <input type="checkbox" checked />
+                          ) : (
+                            <input
+                              type="checkbox"
+                              checked={attributes.permissions.includes(
+                                permission.id,
                               )}
-                            </td>
-                          ))}
-                        </tr>
+                            />
+                          )}
+                        </td>
                       ))}
-                    </Fragment>
-                  ),
-              )}
-            </tbody>
-          </table>
-        )}
-      </Fragment>
+                    </tr>
+                  ))}
+                </Fragment>
+              ),
+          )}
+        </tbody>
+      </table>
     );
   }
 };
