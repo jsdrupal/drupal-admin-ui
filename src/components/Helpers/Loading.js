9cbe92230b2b6bbe128b252860b3197c10756772
Matthew Grill <matt.grill@acquia.com>
2018-03-15 11:26:25 -0700

    Updated Peace-Wave loading component.
    Co-authored-by: Michael Herchel <mike@lullabot.com>

--- src/components/Helpers/Loading.js
+++ src/components/Helpers/Loading.js
@@ -5,28 +5,32 @@ const styles = {
   wrap: css`
     margin: 100px auto 0;
   `,
-  left: css`
-    ::before {
-      content: '⬅️';
-    }
-  `,
-  right: css`
-    ::before {
-      content: '➡️';
-    }
-  `,
   peace: css`
-    ::before {
-      content: '✌️';
+    display: inline-block;
+    vertical-align: middle;
+    animation-direction: alternate;
+    animation-iteration-count: infinite;
+    animation-duration: 0.5s;
+    animation-timing-function: cubic-bezier(0, 0, 1, 1);
+    transform-origin: bottom;
+    font-size: 50px;
+    animation-name: rotate;
+    @keyframes rotate {
+      from {
+        transform: rotate(-10deg);
+      }
+      to {
+        transform: rotate(10deg);
+      }
     }
   `,
 };
 
 const Loading = () => (
   <div className={styles.wrap}>
-    <span className={styles.left} />
-    <span className={styles.peace} />
-    <span className={styles.right} />
+    <span className={styles.peace} role="img" aria-label="Peace Sign">
+      ✌️
+    </span>
   </div>
 );
 
