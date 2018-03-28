a88780c93d6ec6efe1bedacc5b7f04cdc3b395cf
Ted Bowman <ted@tedbow.com>
2018-03-27 22:42:53 -0400

    Return a reason with access forbidden result

--- admin_ui_support/src/ReadOnlyAccessControlHandler.php
+++ admin_ui_support/src/ReadOnlyAccessControlHandler.php
@@ -3,9 +3,11 @@
 namespace Drupal\admin_ui_support;
 
 use Drupal\Core\Access\AccessResult;
+use Drupal\Core\Access\AccessResultForbidden;
 use Drupal\Core\Entity\EntityAccessControlHandler;
 use Drupal\Core\Entity\EntityInterface;
 use Drupal\Core\Session\AccountInterface;
+use Drupal\Tests\Core\Access\AccessResultForbiddenTest;
 
 /**
  * Access controller for the Read only entity types.
@@ -21,14 +23,24 @@ class ReadOnlyAccessControlHandler extends EntityAccessControlHandler {
     if ($operation === 'view') {
       return AccessResult::allowedIfHasPermission($account, $entity->getEntityType()->getAdminPermission());
     }
-    return AccessResult::forbidden();
+    return $this->getForbiddenAccessResult();
   }
 
   /**
    * {@inheritdoc}
    */
   protected function checkCreateAccess(AccountInterface $account, array $context, $entity_bundle = NULL) {
-    return AccessResult::forbidden();
+    return $this->getForbiddenAccessResult();
+  }
+
+  /**
+   * Gets the forbidden access result with a reason.
+   *
+   * @return \Drupal\Core\Access\AccessResultForbidden
+   *   The forbidden acess result.
+   */
+  protected function getForbiddenAccessResult() {
+    return AccessResultForbidden::forbidden('Read only entity type: ' . $this->entityTypeId);
   }
 
 }
