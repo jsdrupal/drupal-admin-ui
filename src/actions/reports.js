383c6c849daf415b2e48057a783d0347f1911c41
Matthew Grill <mattgrill@users.noreply.github.com>
2018-04-24 09:44:18 -0700

    Dblog component (#78)
    * DBLog
    
    * Restructure state, add type select box.
    
    * Cleanup
    
    * Filter by severity
    
    * wip
    
    * Fixes lint errors.
    
    * previous/next buttons
    
    * type filter
    
    * Refactor some helper functions
    
    * Expand test coverage + redirect to dblog (#98)
    
    * Add redirect support
    
    * Write some saga tests
    
    * Fix linting issues

---
+++ src/actions/reports.js
@@ -0,0 +1,96 @@
+import { put, call, takeLatest } from 'redux-saga/effects';
+import {
+  showLoading,
+  hideLoading,
+  resetLoading,
+} from 'react-redux-loading-bar';
+import api from '../utils/api/api';
+import { setMessage } from './application';
+
+export const DBLOG_COLLECTION_REQUEST = 'DBLOG_COLLECTION_REQUEST';
+export const requestDblogCollection = options => ({
+  type: DBLOG_COLLECTION_REQUEST,
+  payload: { options },
+});
+
+export const DBLOG_FILTER_UPDATED = 'DBLOG_FILTER_UPDATED';
+
+export const DBLOG_COLLECTION_LOADED = 'DBLOG_COLLECTION_LOADED';
+export function* loadDblog({ payload: { options } }) {
+  try {
+    const queryString = {
+      sort: options.sort || '',
+      page: {
+        limit: 50,
+        ...(options.offset
+          ? {
+              offset: options.offset,
+            }
+          : {}),
+      },
+      filter: {
+        ...(options.types && Object.keys(options.types).length
+          ? options.types.reduce(
+              (acc, cur) => ({
+                ...acc,
+                [`type${cur}`]: {
+                  condition: {
+                    value: cur,
+                    path: 'type',
+                    memberOf: 'typeGroup',
+                  },
+                },
+              }),
+              { typeGroup: { group: { conjunction: 'OR' } } },
+            )
+          : {}),
+        ...(options.severities && Object.keys(options.severities).length
+          ? options.severities.reduce(
+              (acc, cur) => ({
+                ...acc,
+                [`severity${cur}`]: {
+                  condition: {
+                    value: cur,
+                    path: 'severity',
+                    memberOf: 'severityGroup',
+                  },
+                },
+              }),
+              {
+                severityGroup: {
+                  group: {
+                    conjunction: 'OR',
+                  },
+                },
+              },
+            )
+          : {}),
+      },
+    };
+    yield put(resetLoading());
+    yield put(showLoading());
+    yield put({
+      type: DBLOG_FILTER_UPDATED,
+      payload: {
+        options,
+      },
+    });
+    const dbLogEntriesTypes = yield call(api, 'dblog:types');
+    const dbLogEntries = yield call(api, 'dblog', { queryString });
+    yield put({
+      type: DBLOG_COLLECTION_LOADED,
+      payload: {
+        dbLogEntries,
+        dbLogEntriesTypes,
+      },
+    });
+  } catch (error) {
+    yield put(setMessage(error.toString()));
+  } finally {
+    yield put(hideLoading());
+  }
+}
+
+export const watchDblogRequests = function* watchDblogRequests() {
+  yield takeLatest(DBLOG_COLLECTION_REQUEST, loadDblog);
+};
