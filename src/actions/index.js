298382b666cb520edfd8011c20bf2ec70d35eb94
Lauri Eskola <lauri.eskola@gmail.com>
2018-04-25 12:22:52 +0300

    Fix broken merge

--- src/actions/index.js
+++ src/actions/index.js
@@ -9,7 +9,6 @@ export default function* rootSaga() {
     applicationSaga(),
     watchDblogRequests(),
     watchRequestedRolesWithCancel(),
-    watchRequestedMenu(),
     watchRequestedPermissionsWithCancel(),
   ]);
 }
