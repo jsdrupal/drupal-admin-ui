969e15a9b524488d34fea73c75db0f9729ee0037
Ted Bowman <ted@tedbow.com>
2018-04-26 09:08:35 -0400

    add debug exceptions

--- admin_ui_support/src/Routing/RouteSubscriber.php
+++ admin_ui_support/src/Routing/RouteSubscriber.php
@@ -31,16 +31,35 @@ class RouteSubscriber extends RouteSubscriberBase {
    */
   protected function alterRoutes(RouteCollection $collection) {
     if (!$this->configFactory->get('admin_ui_support.settings')->get('redirect_related_routes')) {
+      throw new \Exception('redirect_related_routes no enabled');
       return;
     }
+    $test_route = 'user.admin_permissions';
+    $test_route_found = FALSE;
     // Find all additional routes that this module should take over by checking
     // the '_admin_related_route' route option.
     foreach ($collection->getIterator() as $override_route_name => $route) {
       if ($overridden_route_name = $route->getOption('_admin_related_route')) {
         if ($overridden_route = $collection->get($overridden_route_name)) {
           $overridden_route->setDefault('_controller', 'Drupal\admin_ui_support\Controller\DefaultController::getAppRoute');
+          if ($overridden_route_name == $test_route) {
+           $test_route_found = TRUE;
+          }
+        }
+        else {
+          if ($overridden_route_name == $test_route) {
+            throw new \Exception("$test_route was not found");
+          }
         }
       }
+      else {
+        if ($override_route_name  == 'admin_ui_support.user.permissions') {
+          throw new \Exception('admin_ui_support.user.permissions should have _admin_related_route option');
+        }
+      }
+    }
+    if (!$test_route_found) {
+      throw new \Exception("$test_route not in loop");
     }
   }
 
