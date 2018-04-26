fc15617c0abd7baf375c4a6490b89ac2a60ce426
Ted Bowman <ted@tedbow.com>
2018-04-26 09:47:36 -0400

    make a request to trigger rebuild

--- admin_ui_support/tests/src/Functional/RedirectRoutesTest.php
+++ admin_ui_support/tests/src/Functional/RedirectRoutesTest.php
@@ -59,6 +59,8 @@ class RedirectRoutesTest extends BrowserTestBase {
     if (\Drupal::configFactory()->get('admin_ui_support.settings')->get('redirect_related_routes')) {
       $this->fail('redirect_related_routes should be disable after form submitted');
     }
+    // Make a request to trigger route rebuild.
+    $this->drupalGet('user');
     // After the setting is updated the paths should go to the default page.
     /** @var \Drupal\Core\Routing\RouteProvider $route_provider */
     $route_provider = \Drupal::service('router.route_provider');
