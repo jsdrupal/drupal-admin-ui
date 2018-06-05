e347299a47e40ebbd8b801eb3d5ddbfcfea297ce
Lauri Eskola <lauri.eskola@gmail.com>
2018-06-05 17:33:39 +0300

    Remove react-burger-menu

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,7 +1,6 @@
 import { loadingBarReducer as loadingBar } from 'react-redux-loading-bar';
-import { reducer as burgerMenu } from 'redux-burger-menu';
 import application from './application';
 import content from './content';
 
-const reducers = { burgerMenu, application, content };
+const reducers = { application, content };
 export default { ...reducers, loadingBar };
