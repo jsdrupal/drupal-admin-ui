b26cb9c7941d798383611975536111a91327a634
Matthew Grill <mattgrill@users.noreply.github.com>
2018-06-14 01:43:46 -0700

    Sort table contents.  (#124)
    * table-sorting
    
    * less complicated
    
    * less drupal

--- src/components/05_pages/Content/Content.js
+++ src/components/05_pages/Content/Content.js
@@ -21,6 +21,7 @@ import TableBody from '@material-ui/core/TableBody';
 import TableCell from '@material-ui/core/TableCell';
 import TableHead from '@material-ui/core/TableHead';
 import TableRow from '@material-ui/core/TableRow';
+import TableSortLabel from '@material-ui/core/TableSortLabel';
 
 import Button from '@material-ui/core/Button';
 import IconButton from '@material-ui/core/IconButton';
@@ -87,11 +88,17 @@ class Content extends Component {
   state = {
     contentTypes: [],
     status: null,
+    sort: { path: 'changed', direction: 'DESC' },
   };
   componentDidMount() {
     this.props.requestContentTypes();
-    this.props.requestContent();
+    this.props.requestContent(this.state);
   }
+  tableSortHandler = (path, direction) => () => {
+    this.setState({ sort: { path, direction } }, () => {
+      this.props.requestContent(this.state);
+    });
+  };
   render = () => (
     <Fragment>
       <Paper>
@@ -179,13 +186,37 @@ class Content extends Component {
           <TableHead>
             <TableRow>
               {[
-                'Title',
-                'Content Type',
-                'Author',
-                'Status',
-                'Updated',
-                'Actions',
-              ].map(item => <TableCell key={item}>{item}</TableCell>)}
+                { key: 'title', label: 'Title', sortable: true },
+                { key: 'type', label: 'Content Type', sortable: true },
+                { key: 'author', label: 'Author', sortable: false },
+                { key: 'status', label: 'Status', sortable: true },
+                { key: 'changed', label: 'Updated', sortable: true },
+                { key: 'operations', label: 'Operations', sortable: false },
+              ].map(
+                ({ key, label, sortable }) =>
+                  sortable ? (
+                    <TableCell key={key}>
+                      <TableSortLabel
+                        direction={
+                          this.state.sort.path === key
+                            ? this.state.sort.direction.toLowerCase()
+                            : undefined
+                        }
+                        active={this.state.sort.path === key}
+                        onClick={this.tableSortHandler(
+                          key,
+                          (this.state.sort.path !== key && 'DESC') ||
+                            ((this.state.sort.direction === 'DESC' && 'ASC') ||
+                              'DESC'),
+                        )}
+                      >
+                        {label}
+                      </TableSortLabel>
+                    </TableCell>
+                  ) : (
+                    <TableCell key={key}>{label}</TableCell>
+                  ),
+              )}
             </TableRow>
           </TableHead>
           <TableBody>
