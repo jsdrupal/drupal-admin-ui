ec2effb7f4ccaecc482042ac954f2ddbace16523
Lauri Eskola <lauri.eskola@gmail.com>
2018-06-25 12:26:47 +0300

    Allow closing drawer

--- src/components/06_wrappers/Default/Default.js
+++ src/components/06_wrappers/Default/Default.js
@@ -7,6 +7,7 @@ import { Link } from 'react-router-dom';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import Drawer from '@material-ui/core/Drawer';
 import List from '@material-ui/core/List';
+import IconButton from '@material-ui/core/IconButton';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemIcon from '@material-ui/core/ListItemIcon';
 import ListItemText from '@material-ui/core/ListItemText';
@@ -19,6 +20,8 @@ import SettingsIcon from '@material-ui/icons/Settings';
 import PeopleIcon from '@material-ui/icons/People';
 import AssesmentIcon from '@material-ui/icons/Assessment';
 import HelpIcon from '@material-ui/icons/Help';
+import MenuIcon from '@material-ui/icons/Menu';
+import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
 
 import { requestMenu } from '../../../actions/application';
 import Message from '../../02_atoms/Message/Message';
@@ -38,14 +41,50 @@ const iconMap = {
 };
 
 class Default extends React.Component {
+  state = {
+    drawerOpen: false,
+  };
   componentWillMount() {
     this.props.requestMenu();
   }
+  handleDrawerOpen = () => {
+    this.setState({ drawerOpen: true });
+  };
+  handleDrawerClose = () => {
+    this.setState({ drawerOpen: false });
+  };
 
   render = () => (
     <div className={styles.outerWrapper}>
       <CssBaseline />
-      <Drawer variant="permanent" classes={{ paper: styles.drawerPaper }}>
+      <Drawer
+        variant="permanent"
+        classes={{
+          paper: `${styles.drawerPaper} ${!this.state.drawerOpen &&
+            styles.drawerPaperClose}`,
+        }}
+        open={this.state.drawerOpen}
+      >
+        <div className={styles.menuButtonWrapper}>
+          {this.state.drawerOpen ? (
+            <IconButton
+              onClick={this.handleDrawerClose}
+              className={styles.menuButton}
+            >
+              <ChevronLeftIcon />
+            </IconButton>
+          ) : (
+            <IconButton
+              color="inherit"
+              aria-label="open drawer"
+              onClick={this.handleDrawerOpen}
+              className={styles.menuButton}
+            >
+              <MenuIcon />
+            </IconButton>
+          )}
+        </div>
+        <Divider />
         <List>
           {this.props.menuLinks.map(({ link: menuLink }) => (
             <ListItem button component={Link} to={menuLink.url}>
@@ -58,7 +97,7 @@ class Default extends React.Component {
             </ListItem>
           ))}
         </List>
-        <Divider />
+        {this.props.menuLinks.length ? <Divider /> : ''}
       </Drawer>
 
       <main className={styles.main} id={styles.main}>
@@ -72,6 +111,13 @@ class Default extends React.Component {
 }
 
 styles = {
+  menuButton: css`
+    margin: 8px 12px;
+  `,
+  menuButtonWrapper: css`
+    display: flex;
+    justify-content: flex-end;
+  `,
   outerWrapper: css`
     height: 100%;
     display: flex;
@@ -84,6 +130,13 @@ styles = {
   drawerPaper: css`
     position: relative;
     width: 250px;
+    transition: width 225ms cubic-bezier(0.4, 0, 0.6, 1) 0ms;
+  `,
+  drawerPaperClose: css`
+    overflow-x: 'hidden';
+    transition: none;
+    width: 72px;
+    transition: width 195ms cubic-bezier(0.4, 0, 0.6, 1) 0ms;
   `,
 };
 
