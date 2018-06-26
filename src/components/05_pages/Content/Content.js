af2f5889ecc8a4cb5b3d802e8a00f4545edd0f7f
Lauri Eskola <lauri.eskola@gmail.com>
2018-06-26 14:19:24 +0300

    Remove fragment import

--- src/components/05_pages/Content/Content.js
+++ src/components/05_pages/Content/Content.js
@@ -1,4 +1,4 @@
-import React, { Component, Fragment } from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
