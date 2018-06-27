0f2ef72a3889d4f5120f73e4dc2e8e2ed9a03405
Matthew Grill <mattgrill@users.noreply.github.com>
2018-06-27 04:34:31 -0400

    Content list pagination (#137)
    * pagination
    
    * limits at 50
    
    * Use material UI table pagination
    
    * Add some margin-bottom for the table to make sure add button and
    pagination don't overlap
    
    * Remove fragment import
    
    * Show page number
    
    * Add documentation for the scroll to top

--- src/components/05_pages/Content/Content.js
+++ src/components/05_pages/Content/Content.js
@@ -1,4 +1,4 @@
-import React, { Component, Fragment } from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
@@ -22,6 +22,7 @@ import TableCell from '@material-ui/core/TableCell';
 import TableHead from '@material-ui/core/TableHead';
 import TableRow from '@material-ui/core/TableRow';
 import TableSortLabel from '@material-ui/core/TableSortLabel';
+import TablePagination from '@material-ui/core/TablePagination';
 
 import Button from '@material-ui/core/Button';
 import IconButton from '@material-ui/core/IconButton';
@@ -34,8 +35,7 @@ import { requestContent } from '../../../actions/content';
 
 const styles = {
   root: css`
-    display: flex;
-    flex-wrap: wrap;
+    margin-bottom: 50px;
   `,
   addButton: css`
     margin: 0.5rem;
@@ -80,221 +80,288 @@ class Content extends Component {
     includes: PropTypes.shape({
       'user--user': PropTypes.object,
     }),
+    links: PropTypes.shape({
+      next: PropTypes.string,
+    }),
   };
   static defaultProps = {
     contentList: [],
     includes: {},
+    links: {},
   };
   state = {
     contentTypes: [],
+    scrollToTop: false,
     status: null,
     sort: { path: 'changed', direction: 'DESC' },
+    page: {
+      offset: 0,
+      limit: 50,
+    },
   };
   componentDidMount() {
     this.props.requestContentTypes();
     this.props.requestContent(this.state);
   }
+  componentDidUpdate(prevProps) {
+    // Operations executed in the bottom of the page should scroll back to top
+    // when list content changes. For example, the pagination uses this.
+    if (
+      this.state.scrollToTop &&
+      JSON.stringify(prevProps.contentList) !==
+        JSON.stringify(this.props.contentList)
+    ) {
+      window.scrollTo(0, this.table.offsetTop);
+    }
+  }
   tableSortHandler = (path, direction) => () => {
-    this.setState({ sort: { path, direction } }, () => {
-      this.props.requestContent(this.state);
-    });
+    this.setState(
+      {
+        sort: { path, direction },
+        page: { offset: 0, limit: this.state.page.limit },
+      },
+      () => {
+        this.props.requestContent(this.state);
+      },
+    );
   };
-  render = () => (
-    <Fragment>
-      <Paper>
-        <div className={styles.filters}>
-          <TextField
-            label="Title"
-            placeholder="Title"
-            onChange={e => {
-              this.setState({ title: e.target.value }, () => {
-                this.props.requestContent(this.state);
-              });
-            }}
-            margin="normal"
-          />
+  pageChangeHandler = (event, page) => {
+    this.setState(
+      ({ page: { limit } }) => ({
+        page: { offset: page * limit, limit },
+        scrollToTop: true,
+      }),
+      () => {
+        this.props.requestContent(this.state);
+      },
+    );
+  };
 
-          <FormControl className={styles.formControl}>
-            <InputLabel htmlFor="select-multiple-checkbox">
-              Content Type
-            </InputLabel>
-            <Select
-              multiple
-              value={this.state.contentTypes}
+  render = () => {
+    const { page: { offset, limit } } = this.state;
+    const { links, contentList } = this.props;
+
+    // Calculate the highest known count.
+    const count = offset + contentList.length + (links.next ? 1 : 0);
+
+    return (
+      <div className={styles.root}>
+        <Paper>
+          <div className={styles.filters}>
+            <TextField
+              label="Title"
+              placeholder="Title"
               onChange={e => {
-                this.setState({ contentTypes: e.target.value }, () => {
+                this.setState({ title: e.target.value }, () => {
                   this.props.requestContent(this.state);
                 });
               }}
-              input={<Input id="select-multiple-checkbox" />}
-              renderValue={selected => (
-                <div className={styles.chips}>
-                  {selected.map(value => (
-                    <Chip
-                      key={value}
-                      label={this.props.contentTypes[value].name}
-                      className={styles.chip}
+              margin="normal"
+            />
+
+            <FormControl className={styles.formControl}>
+              <InputLabel htmlFor="select-multiple-checkbox">
+                Content Type
+              </InputLabel>
+              <Select
+                multiple
+                value={this.state.contentTypes}
+                onChange={e => {
+                  this.setState({ contentTypes: e.target.value }, () => {
+                    this.props.requestContent(this.state);
+                  });
+                }}
+                input={<Input id="select-multiple-checkbox" />}
+                renderValue={selected => (
+                  <div className={styles.chips}>
+                    {selected.map(value => (
+                      <Chip
+                        key={value}
+                        label={this.props.contentTypes[value].name}
+                        className={styles.chip}
+                      />
+                    ))}
+                  </div>
+                )}
+              >
+                {Object.keys(this.props.contentTypes).map(type => (
+                  <MenuItem key={type} value={type}>
+                    <Checkbox
+                      checked={this.state.contentTypes.indexOf(type) > -1}
                     />
-                  ))}
-                </div>
-              )}
-            >
-              {Object.keys(this.props.contentTypes).map(type => (
-                <MenuItem key={type} value={type}>
-                  <Checkbox
-                    checked={this.state.contentTypes.indexOf(type) > -1}
-                  />
-                  <ListItemText primary={this.props.contentTypes[type].name} />
+                    <ListItemText
+                      primary={this.props.contentTypes[type].name}
+                    />
+                  </MenuItem>
+                ))}
+              </Select>
+            </FormControl>
+
+            <FormControl className={styles.formControl}>
+              <InputLabel htmlFor="status">Status</InputLabel>
+              <Select
+                value={this.state.status || ''}
+                onChange={e => {
+                  this.setState({ status: e.target.value }, () => {
+                    this.props.requestContent(this.state);
+                  });
+                }}
+                input={<Input name="status" id="status" />}
+                autoWidth
+              >
+                <MenuItem value="">
+                  <em>Any</em>
                 </MenuItem>
-              ))}
-            </Select>
-          </FormControl>
+                <MenuItem value="published">Published</MenuItem>
+                <MenuItem value="unpublished">Unpublished</MenuItem>
+              </Select>
+            </FormControl>
 
-          <FormControl className={styles.formControl}>
-            <InputLabel htmlFor="status">Status</InputLabel>
-            <Select
-              value={this.state.status || ''}
-              onChange={e => {
-                this.setState({ status: e.target.value }, () => {
-                  this.props.requestContent(this.state);
-                });
-              }}
-              input={<Input name="status" id="status" />}
-              autoWidth
+            <Button
+              variant="fab"
+              color="primary"
+              aria-label="add"
+              className={styles.addButton}
+              component={Link}
+              to="/node/add"
             >
-              <MenuItem value="">
-                <em>Any</em>
-              </MenuItem>
-              <MenuItem value="published">Published</MenuItem>
-              <MenuItem value="unpublished">Unpublished</MenuItem>
-            </Select>
-          </FormControl>
+              <AddIcon />
+            </Button>
+          </div>
 
-          <Button
-            variant="fab"
-            color="primary"
-            aria-label="add"
-            className={styles.addButton}
-            component={Link}
-            to="/node/add"
+          <div
+            ref={node => {
+              this.table = node;
+            }}
           >
-            <AddIcon />
-          </Button>
-        </div>
-
-        <Table>
-          <TableHead>
-            <TableRow>
-              {[
-                { key: 'title', label: 'Title', sortable: true },
-                { key: 'type', label: 'Content Type', sortable: true },
-                { key: 'author', label: 'Author', sortable: false },
-                { key: 'status', label: 'Status', sortable: true },
-                { key: 'changed', label: 'Updated', sortable: true },
-                { key: 'operations', label: 'Operations', sortable: false },
-              ].map(
-                ({ key, label, sortable }) =>
-                  sortable ? (
-                    <TableCell key={key}>
-                      <TableSortLabel
-                        direction={
-                          this.state.sort.path === key
-                            ? this.state.sort.direction.toLowerCase()
-                            : undefined
-                        }
-                        active={this.state.sort.path === key}
-                        onClick={this.tableSortHandler(
-                          key,
-                          (this.state.sort.path !== key && 'DESC') ||
-                            ((this.state.sort.direction === 'DESC' && 'ASC') ||
-                              'DESC'),
-                        )}
-                      >
-                        {label}
-                      </TableSortLabel>
-                    </TableCell>
-                  ) : (
-                    <TableCell key={key}>{label}</TableCell>
-                  ),
-              )}
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            {this.props.contentList.map(
-              ({
-                type,
-                attributes: { changed, nid, status, title },
-                relationships,
-              }) => (
-                <TableRow key={nid}>
-                  <TableCell>{title}</TableCell>
-                  <TableCell>{this.props.contentTypes[type].name}</TableCell>
-                  <TableCell>
-                    {this.props.includes['user--user'][
-                      relationships.uid.data.id
-                    ] ? (
-                      <Link
-                        to={`/user/${
-                          this.props.includes['user--user'][
-                            relationships.uid.data.id
-                          ].attributes.uid
-                        }`}
-                      >
-                        {
-                          this.props.includes['user--user'][
-                            relationships.uid.data.id
-                          ].attributes.name
-                        }
-                      </Link>
-                    ) : (
-                      'Anonymous (not verified)'
-                    )}
-                  </TableCell>
-                  <TableCell>
-                    {(status && 'Published') || 'Unpublished'}
-                  </TableCell>
-                  <TableCell>
-                    {new Intl.DateTimeFormat(navigator.language, {
-                      weekday: 'long',
-                      year: 'numeric',
-                      month: 'long',
-                      day: 'numeric',
-                      hour: 'numeric',
-                      minute: 'numeric',
-                    }).format(new Date(changed * 1000))}
-                  </TableCell>
-                  <TableCell style={{ whiteSpace: 'nowrap' }}>
-                    <IconButton
-                      aria-label="edit"
-                      className={styles.button}
-                      component={Link}
-                      to={`/node/${nid}/edit`}
-                    >
-                      <EditIcon />
-                    </IconButton>
-                    <IconButton
-                      aria-label="delete"
-                      className={styles.button}
-                      component={Link}
-                      to={`/node/${nid}/delete`}
-                    >
-                      <DeleteIcon />
-                    </IconButton>
-                  </TableCell>
+            <Table>
+              <TableHead>
+                <TableRow>
+                  {[
+                    { key: 'title', label: 'Title', sortable: true },
+                    { key: 'type', label: 'Content Type', sortable: true },
+                    { key: 'author', label: 'Author', sortable: false },
+                    { key: 'status', label: 'Status', sortable: true },
+                    { key: 'changed', label: 'Updated', sortable: true },
+                    { key: 'operations', label: 'Operations', sortable: false },
+                  ].map(
+                    ({ key, label, sortable }) =>
+                      sortable ? (
+                        <TableCell key={key}>
+                          <TableSortLabel
+                            direction={
+                              this.state.sort.path === key
+                                ? this.state.sort.direction.toLowerCase()
+                                : undefined
+                            }
+                            active={this.state.sort.path === key}
+                            onClick={this.tableSortHandler(
+                              key,
+                              (this.state.sort.path !== key && 'DESC') ||
+                                ((this.state.sort.direction === 'DESC' &&
+                                  'ASC') ||
+                                  'DESC'),
+                            )}
+                          >
+                            {label}
+                          </TableSortLabel>
+                        </TableCell>
+                      ) : (
+                        <TableCell key={key}>{label}</TableCell>
+                      ),
+                  )}
                 </TableRow>
-              ),
-            )}
-          </TableBody>
-        </Table>
-      </Paper>
-    </Fragment>
-  );
+              </TableHead>
+              <TableBody>
+                {this.props.contentList.map(
+                  ({
+                    type,
+                    attributes: { changed, nid, status, title },
+                    relationships,
+                  }) => (
+                    <TableRow key={nid}>
+                      <TableCell>{title}</TableCell>
+                      <TableCell>
+                        {this.props.contentTypes[type].name}
+                      </TableCell>
+                      <TableCell>
+                        {this.props.includes['user--user'][
+                          relationships.uid.data.id
+                        ] ? (
+                          <Link
+                            to={`/user/${
+                              this.props.includes['user--user'][
+                                relationships.uid.data.id
+                              ].attributes.uid
+                            }`}
+                          >
+                            {
+                              this.props.includes['user--user'][
+                                relationships.uid.data.id
+                              ].attributes.name
+                            }
+                          </Link>
+                        ) : (
+                          'Anonymous (not verified)'
+                        )}
+                      </TableCell>
+                      <TableCell>
+                        {(status && 'Published') || 'Unpublished'}
+                      </TableCell>
+                      <TableCell>
+                        {new Intl.DateTimeFormat(navigator.language, {
+                          weekday: 'long',
+                          year: 'numeric',
+                          month: 'long',
+                          day: 'numeric',
+                          hour: 'numeric',
+                          minute: 'numeric',
+                        }).format(new Date(changed * 1000))}
+                      </TableCell>
+                      <TableCell style={{ whiteSpace: 'nowrap' }}>
+                        <IconButton
+                          aria-label="edit"
+                          className={styles.button}
+                          component={Link}
+                          to={`/node/${nid}/edit`}
+                        >
+                          <EditIcon />
+                        </IconButton>
+                        <IconButton
+                          aria-label="delete"
+                          className={styles.button}
+                          component={Link}
+                          to={`/node/${nid}/delete`}
+                        >
+                          <DeleteIcon />
+                        </IconButton>
+                      </TableCell>
+                    </TableRow>
+                  ),
+                )}
+              </TableBody>
+            </Table>
+            <TablePagination
+              component="div"
+              count={count}
+              rowsPerPage={limit}
+              page={offset / limit}
+              onChangePage={this.pageChangeHandler}
+              rowsPerPageOptions={[limit]}
+              labelDisplayedRows={({ page }) => `Page: ${page + 1}`}
+            />
+          </div>
+        </Paper>
+      </div>
+    );
+  };
 }
 
 const mapStateToProps = state => ({
   contentTypes: state.application.contentTypes,
   contentList: state.content.contentList,
   includes: state.content.includes,
+  links: state.content.links,
 });
 
 export default connect(mapStateToProps, {
