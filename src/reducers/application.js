fe2d98c2ca493f395d9b3d9df7492c063ba294fd
Lauri Eskola <lauri.eskola@gmail.com>
2018-06-27 15:44:01 +0700

    Allow closing drawer (#140)
    * Allow closing drawer
    
    * Use redux store instead of component state

--- src/reducers/application.js
+++ src/reducers/application.js
@@ -5,6 +5,8 @@ import {
   DBLOG_FILTER_UPDATED,
 } from '../actions/reports';
 import {
+  CLOSE_DRAWER,
+  OPEN_DRAWER,
   SET_MESSAGE,
   CLEAR_MESSAGE,
   MENU_LOADED,
@@ -16,9 +18,22 @@ export const initialState = {
   menuLinks: [],
   filterString: '',
   contentTypes: {},
+  drawerOpen: false,
 };
 export default (state = initialState, action) => {
   switch (action.type) {
+    case CLOSE_DRAWER: {
+      return {
+        ...state,
+        drawerOpen: false,
+      };
+    }
+    case OPEN_DRAWER: {
+      return {
+        ...state,
+        drawerOpen: true,
+      };
+    }
     case SET_MESSAGE: {
       return {
         ...state,
