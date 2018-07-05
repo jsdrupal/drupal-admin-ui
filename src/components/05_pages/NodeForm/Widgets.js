ea0a6f006c777420f9855d2f4d0a6a8bd34edefb
Volker Killesreiter <killesre@gmail.com>
2018-07-05 17:29:40 +0100

    Initial number widget (#163)
    * intial number widget
    
    * add some inputProps

--- src/components/05_pages/NodeForm/Widgets.js
+++ src/components/05_pages/NodeForm/Widgets.js
@@ -1,7 +1,9 @@
+import NumberTextfield from '../../02_atoms/Widgets/NumberTextfield';
 import StringTextfield from '../../02_atoms/Widgets/StringTextfield';
 
 // @todo How do we do extensibility from modules, maybe a build step?
 const widgets = {
+  number_textfield: NumberTextfield,
   string_textfield: StringTextfield,
 };
 
