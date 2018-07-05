f63bb11cdb838bbf762fb5f08b89ef365e8ac866
Ricardo Sanz <ricardo@metadrop.net>
2018-07-05 16:57:27 +0100

    Add a comma to make eslint happier.

--- src/components/05_pages/NodeForm/Widgets.js
+++ src/components/05_pages/NodeForm/Widgets.js
@@ -4,7 +4,7 @@ import BooleanCheckbox from '../../02_atoms/Widgets/BooleanCheckbox';
 // @todo How do we do extensibility from modules, maybe a build step?
 const widgets = {
   string_textfield: StringTextfield,
-  boolean_checkbox: BooleanCheckbox
+  boolean_checkbox: BooleanCheckbox,
 };
 
 export default widgets;
