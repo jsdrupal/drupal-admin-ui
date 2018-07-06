a0fb47c4497f8ca45174dafb555dff79e24b7a1b
Sally Young <sally@justafish.co.uk>
2018-07-06 10:11:48 +0100

    [#52] debugging

--- src/tests/Nightwatch/Tests/menu.js
+++ src/tests/Nightwatch/Tests/menu.js
@@ -8,6 +8,7 @@ module.exports = {
       .getText('[data-nightwatch="menu"] a[role="button"]', function menuText(
         result,
       ) {
+        console.log(result);
         this.assert.strictEqual(result.value, 'Content');
       });
   },
