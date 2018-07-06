2793fcab62d1897efc21be5bb359b286f52b6b10
Ricardo Sanz <ricardo@metadrop.net>
2018-07-06 16:12:38 +0100

    Improve BooleanCheckbox: use label, pass additional props, fix defiend props.

--- src/components/02_atoms/Widgets/BooleanCheckbox.js
+++ src/components/02_atoms/Widgets/BooleanCheckbox.js
@@ -1,27 +1,37 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import CheckBox from '@material-ui/core/Checkbox';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
 import WidgetPropTypes from '../../05_pages/NodeForm/WidgetPropTypes';
 
-const BooleanCheckbox = props => (
-  <CheckBox
-    id={props.fieldName}
-    checked={props.checked}
-    onChange={event => props.onChange(event.target.value)}
-    margin="normal"
-    label={props.label}
-  />
-);
+const BooleanCheckbox = props => {
+  const { onChange, label, id, value, ...checkboxProps } = props;
+
+  return (
+    <FormControlLabel
+      id={`${props.fieldName}-label`}
+      control={
+        <CheckBox
+          id={`${props.fieldName}-cb`}
+          checked={value}
+          onChange={event => onChange(event.target.value)}
+          margin="normal"
+          value={value}
+          {...checkboxProps}
+        />
+      }
+      label={label}
+    />
+  );
+};
 
 BooleanCheckbox.propTypes = {
   ...WidgetPropTypes,
-  checked: PropTypes.bool,
-  label: PropTypes.string,
+  value: PropTypes.bool,
 };
 
 BooleanCheckbox.defaultProps = {
-  checked: false,
-  label: 'On',
+  value: false,
 };
 
 export default BooleanCheckbox;
