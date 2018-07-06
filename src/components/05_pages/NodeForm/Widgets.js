5e5689b60da3d53ba9908ab5ebaddb91a0c93df2
Vijay Dubb <VD39@users.noreply.github.com>
2018-07-06 15:40:12 +0200

    Added TimestampDatetime component (#167)
    * Added TimestampDatetime component
    
    * Updated prop value
    
    * Added onChange prop

--- src/components/05_pages/NodeForm/Widgets.js
+++ src/components/05_pages/NodeForm/Widgets.js
@@ -1,10 +1,12 @@
 import NumberTextfield from '../../02_atoms/Widgets/NumberTextfield';
 import StringTextfield from '../../02_atoms/Widgets/StringTextfield';
+import TimestampDatetime from '../../02_atoms/Widgets/TimestampDatetime';
 
 // @todo How do we do extensibility from modules, maybe a build step?
 const widgets = {
   number_textfield: NumberTextfield,
   string_textfield: StringTextfield,
+  timestamp_datetime: TimestampDatetime,
 };
 
 export default widgets;
