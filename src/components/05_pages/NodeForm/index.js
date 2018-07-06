c46937b38310783932f76e8fb4eb115ec8728d5e
Daniel Wehner <dawehner@googlemail.com>
2018-07-06 10:54:23 +0100

    Fix failure

--- src/components/05_pages/NodeForm/index.js
+++ src/components/05_pages/NodeForm/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import withStyles from '@material-ui/core/es/styles/withStyles';
+import { withStyles } from '@material-ui/core';
 import Widgets from './Widgets';
 
 const lazyFunction = f => (props, propName, componentName, ...rest) =>
