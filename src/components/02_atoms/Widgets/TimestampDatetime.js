b77ed33efffdd0fd8501d89ced845d0b13f0eae1
Vijay Dubb <VD39@users.noreply.github.com>
2018-07-06 14:51:41 +0200

    Added onChange prop

--- src/components/02_atoms/Widgets/TimestampDatetime.js
+++ src/components/02_atoms/Widgets/TimestampDatetime.js
@@ -3,12 +3,13 @@ import PropTypes from 'prop-types';
 import TextField from '@material-ui/core/TextField';
 import WidgetPropTypes from '../../05_pages/NodeForm/WidgetPropTypes';
 
-const TimestampDatetime = ({ fieldName, label, value }) => (
+const TimestampDatetime = ({ fieldName, label, value, onChange }) => (
   <TextField
     id={fieldName}
     label={label}
     type="datetime-local"
     defaultValue={value}
+    onChange={event => onChange(event.target.value)}
     InputLabelProps={{
       shrink: true,
     }}
