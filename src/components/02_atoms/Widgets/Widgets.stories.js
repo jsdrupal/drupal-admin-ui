24c4e74ccd1dda591326bc3c0252f942bbe0f5a8
martin <martinfrances107@hotmail.com>
2018-07-11 19:53:17 +0100

    Added default story entries for new widgets [ OptionsSelect and TimestampDatetime]. Removed many errors shown in console.log pannel.

--- src/components/02_atoms/Widgets/Widgets.stories.js
+++ src/components/02_atoms/Widgets/Widgets.stories.js
@@ -2,27 +2,77 @@ import React from 'react';
 // eslint-disable-next-line import/no-extraneous-dependencies
 import { storiesOf } from '@storybook/react';
 
-import FormControlLabel from '@material-ui/core/FormControlLabel';
 import BooleanCheckbox from './BooleanCheckbox';
 import FileUploadWidget from './FileUploadWidget';
 import NumberTextfield from './NumberTextfield';
+import OptionsSelect from './OptionsSelect';
 import StringTextfield from './StringTextfield';
+import TimestampDatetime from './TimestampDatetime';
+
+const mockOnChange = e => {
+  e.preventDefault();
+};
 
 storiesOf('Widgets/BooleanCheckbox', module).addWithJSX('Default', () => (
-  <FormControlLabel control={<BooleanCheckbox />} label="Checkbox label" />
+  <BooleanCheckbox
+    fieldName="ControlOne"
+    label="CheckBox"
+    onChange={mockOnChange}
+  />
 ));
 storiesOf('Widgets/FileUploadWidget', module).addWithJSX('Default', () => (
-  <FileUploadWidget />
+  <FileUploadWidget
+    bundle="node"
+    entityTypeId="Article"
+    fieldName="image-file"
+    label="File to be uploaded"
+    onChange={mockOnChange}
+    value={{
+      data: { uuid: [{ value: '1234' }] },
+      meta: { alt: 'This is an alternative.' },
+    }}
+  />
 ));
+
 storiesOf('Widgets/NumberTextfield', module).addWithJSX('Default', () => (
   <NumberTextfield
-    min="0"
-    max="9"
-    step="1"
-    value="5"
+    fieldName="textField"
     label="0-9 in steps of 1"
+    inputProps={{
+      min: 0,
+      max: 9,
+      step: 1,
+      value: 5,
+    }}
+    onChange={mockOnChange}
+  />
+));
+
+storiesOf('Widgets/OptionsSelect', module).addWithJSX('Default', () => (
+  <OptionsSelect
+    fieldName="option"
+    helpText="Help text."
+    label="A Simple Label"
+    onChange={mockOnChange}
+    schema={{ enum: ['One', 'Two', 'Three', 'Four'], default: 'Two' }}
+    value="Entered text."
   />
 ));
 storiesOf('Widgets/StringTextfield', module).addWithJSX('Default', () => (
-  <StringTextfield value="Entered text." label="A Simple label." />
+  <StringTextfield
+    fieldName="userBio"
+    label="A Simple Label"
+    onChange={mockOnChange}
+    value="Entered text."
+  />
+));
+
+storiesOf('Widgets/TimestampDatetime', module).addWithJSX('Default', () => (
+  <TimestampDatetime
+    fieldName="EventStart"
+    label="A Simple Label"
+    name="startTime"
+    onChange={mockOnChange}
+    value="0"
+  />
 ));
