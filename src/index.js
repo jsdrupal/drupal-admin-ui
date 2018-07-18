555bb4472ef0ea863b458c10391ec9f4f213bd0d
martin frances <martinfrances107@hotmail.com>
2018-07-18 10:17:20 +0100

    Improve reporting of  a11y issues [lint checks and react-axe ] ( #109 ) (#192)
    * Use react-axe from a11y ( #109 )
    
    * Fold a11y tests into lint checks.
    
    * Use the property onBlur in favour or onChange.

--- src/index.js
+++ src/index.js
@@ -3,5 +3,11 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
+if (process.env.NODE_ENV !== 'production') {
+  // eslint-disable-next-line import/no-extraneous-dependencies, global-require
+  const axe = require('react-axe');
+  axe(React, ReactDOM);
+}
+
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
