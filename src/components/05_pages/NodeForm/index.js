jsdrupal/drupal-admin-ui

commit 7601b5a8e132ecf71b459ae722bea877e9cc678e

Fetch Schema Automatically (#222)
* fetch schema automatically

* Store all the schema.

src/components/05_pages/NodeForm/index.js
@@ -1,10 +1,13 @@
 import React, { Fragment } from 'react';
 import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
 import { css } from 'emotion';
 import Paper from '@material-ui/core/Paper';
 
 import Widgets from './Widgets';
 import PageTitle from '../../02_atoms/PageTitle';
+
+import { requestSchema } from '../../../actions/content';
 
 const lazyFunction = f => (props, propName, componentName, ...rest) =>
   f(props, propName, componentName, ...rest);
@@ -38,10 +41,11 @@ class NodeForm extends React.Component {
     ).isRequired,
     entityTypeId: PropTypes.string.isRequired,
     bundle: PropTypes.string.isRequired,
+    requestSchema: PropTypes.func.isRequired,
   };
 
   static defaultProps = {
-    schema: {},
+    schema: false,
   };
 
   constructor(props) {
@@ -50,6 +54,10 @@ class NodeForm extends React.Component {
     this.state = {
       entity: {},
     };
+  }
+
+  componentDidMount() {
+    this.props.requestSchema();
   }
 
   onFieldChange = fieldName => data => {
@@ -69,36 +77,38 @@ class NodeForm extends React.Component {
     return (
       <Fragment>
         <PageTitle>Create {this.props.bundle}</PageTitle>
-        <Paper>
-          <form className={styles.container}>
-            {Object.entries(this.props.uiMetadata)
-              .map(([fieldName, { widget, inputProps }]) => {
-                if (Widgets[widget]) {
-                  // @todo We need to pass along props.
-                  // @todo How do we handle cardinality together with jsonapi
-                  // making a distinction between single value fields and multi value fields.
-                  const fieldSchema = this.getSchemaInfo(
-                    this.props.schema,
-                    fieldName,
-                  );
+        {this.props.schema && (
+          <Paper>
+            <form className={styles.container}>
+              {Object.entries(this.props.uiMetadata)
+                .map(([fieldName, { widget, inputProps }]) => {
+                  if (Widgets[widget]) {
+                    // @todo We need to pass along props.
+                    // @todo How do we handle cardinality together with jsonapi
+                    // making a distinction between single value fields and multi value fields.
+                    const fieldSchema = this.getSchemaInfo(
+                      this.props.schema,
+                      fieldName,
+                    );
 
-                  return React.createElement(this.props.widgets[widget], {
-                    key: fieldName,
-                    entityTypeId: this.props.entityTypeId,
-                    bundle: this.props.bundle,
-                    fieldName,
-                    value: this.state.entity[fieldName],
-                    label: fieldSchema && fieldSchema.title,
-                    schema: fieldSchema,
-                    onChange: this.onFieldChange(fieldName),
-                    inputProps,
-                  });
-                }
-                return null;
-              })
-              .filter(x => x)}
-          </form>
-        </Paper>
+                    return React.createElement(this.props.widgets[widget], {
+                      key: fieldName,
+                      entityTypeId: this.props.entityTypeId,
+                      bundle: this.props.bundle,
+                      fieldName,
+                      value: this.state.entity[fieldName],
+                      label: fieldSchema && fieldSchema.title,
+                      schema: fieldSchema,
+                      onChange: this.onFieldChange(fieldName),
+                      inputProps,
+                    });
+                  }
+                  return null;
+                })
+                .filter(x => x)}
+            </form>
+          </Paper>
+        )}
       </Fragment>
     );
   }
@@ -112,4 +122,13 @@ styles = {
   `,
 };
 
-export default NodeForm;
+const mapStateToProps = (state, { bundle, entityTypeId }) => ({
+  schema: state.content.schema[`${entityTypeId}--${bundle}`],
+});
+
+export default connect(
+  mapStateToProps,
+  {
+    requestSchema,
+  },
+)(NodeForm);

src/actions/content.js
@@ -9,6 +9,33 @@ import api from '../utils/api/api';
 import { MESSAGE_ERROR } from '../constants/messages';
 import { setMessage } from './application';
 
+export const SCHEMA_REQUESTED = 'SCHEMA_REQUESTED';
+export const requestSchema = () => ({
+  type: SCHEMA_REQUESTED,
+  payload: {},
+});
+
+export const SCHEMA_LOADED = 'SCHEMA_LOADED';
+function* loadSchema() {
+  try {
+    yield put(resetLoading());
+    yield put(showLoading());
+
+    // @todo Can we filter this? Fetching the entire schema seems silly.
+    const { definitions: schema } = yield call(api, 'schema');
+    yield put({
+      type: SCHEMA_LOADED,
+      payload: {
+        schema,
+      },
+    });
+  } catch (error) {
+    yield put(setMessage(error.toString(), MESSAGE_ERROR));
+  } finally {
+    yield put(hideLoading());
+  }
+}
+
 export const CONTENT_REQUESTED = 'CONTENT_REQUESTED';
 export const requestContent = (
   options = { contentTypes: [], status: null },
@@ -270,6 +297,7 @@ function* deleteContent({ payload: { content } }) {
 }
 
 export default function* rootSaga() {
+  yield takeLatest(SCHEMA_REQUESTED, loadSchema);
   yield takeLatest(CONTENT_REQUESTED, loadContent);
   yield takeLatest(ACTION_EXECUTE, executeAction);
   yield takeLatest(CONTENT_SAVE, saveContent);

src/components/05_pages/NodeForm/RecipeSchema.js
@@ -1,391 +1,0 @@
-export default {
-  type: 'object',
-  properties: {
-    type: {
-      type: 'string',
-      title: 'type',
-      description: 'Resource type',
-    },
-    id: {
-      type: 'string',
-      title: 'Resource ID',
-      format: 'uuid',
-      maxLength: 128,
-    },
-    attributes: {
-      description: 'Entity attributes',
-      type: 'object',
-      properties: {
-        nid: {
-          type: 'integer',
-          title: 'ID',
-        },
-        uuid: {
-          type: 'string',
-          title: 'UUID',
-          maxLength: 128,
-        },
-        vid: {
-          type: 'integer',
-          title: 'Revision ID',
-        },
-        langcode: {
-          type: 'object',
-          properties: {
-            value: {
-              type: 'string',
-              title: 'Language code',
-            },
-            language: {
-              type: 'language_reference',
-              title: 'Language object',
-              description: 'The referenced language',
-            },
-          },
-          required: ['value'],
-          title: 'Language',
-        },
-        revision_timestamp: {
-          type: 'number',
-          title: 'Revision create time',
-          format: 'utc-millisec',
-          description: 'The time that the current revision was created.',
-        },
-        revision_log: {
-          type: 'string',
-          title: 'Revision log message',
-          description: 'Briefly describe the changes you have made.',
-          default: '',
-        },
-        status: {
-          type: 'boolean',
-          title: 'Published',
-          default: true,
-        },
-        title: {
-          type: 'string',
-          title: 'Recipe Name',
-          maxLength: 255,
-        },
-        created: {
-          type: 'number',
-          title: 'Authored on',
-          format: 'utc-millisec',
-          description: 'The time that the node was created.',
-        },
-        changed: {
-          type: 'number',
-          title: 'Changed',
-          format: 'utc-millisec',
-          description: 'The time that the node was last edited.',
-        },
-        promote: {
-          type: 'boolean',
-          title: 'Promoted to front page',
-          default: true,
-        },
-        sticky: {
-          type: 'boolean',
-          title: 'Sticky at top of lists',
-          default: false,
-        },
-        default_langcode: {
-          type: 'boolean',
-          title: 'Default translation',
-          description:
-            'A flag indicating whether this is the default translation.',
-          default: true,
-        },
-        revision_default: {
-          type: 'boolean',
-          title: 'Default revision',
-          description:
-            'A flag indicating whether this was a default revision when it was saved.',
-        },
-        revision_translation_affected: {
-          type: 'boolean',
-          title: 'Revision translation affected',
-          description:
-            'Indicates if the last edit of a translation belongs to current revision.',
-        },
-        path: {
-          type: 'object',
-          properties: {
-            alias: {
-              type: 'string',
-              title: 'Path alias',
-            },
-            pid: {
-              type: 'integer',
-              title: 'Path id',
-            },
-            langcode: {
-              type: 'string',
-              title: 'Language Code',
-            },
-          },
-          title: 'URL alias',
-        },
-        field_cooking_time: {
-          type: 'integer',
-          title: 'Cooking time',
-        },
-        field_difficulty: {
-          type: 'string',
-          title: 'Difficulty',
-          enum: ['easy', 'medium', 'hard'],
-          default: 'medium',
-        },
-        field_ingredients: {
-          type: 'array',
-          title: 'Ingredients',
-          description:
-            'List the ingredients required for this recipe, one per item.',
-          items: {
-            type: 'string',
-            title: 'Text value',
-            maxLength: 255,
-          },
-        },
-        field_number_of_servings: {
-          type: 'integer',
-          title: 'Number of servings',
-        },
-        field_preparation_time: {
-          type: 'integer',
-          title: 'Preparation time',
-        },
-        field_recipe_instruction: {
-          type: 'object',
-          properties: {
-            value: {
-              type: 'string',
-              title: 'Text',
-            },
-            format: {
-              type: 'string',
-              title: 'Text format',
-            },
-          },
-          required: ['value'],
-          title: 'Recipe instruction',
-        },
-        field_summary: {
-          type: 'object',
-          properties: {
-            value: {
-              type: 'string',
-              title: 'Text',
-            },
-            format: {
-              type: 'string',
-              title: 'Text format',
-            },
-          },
-          required: ['value'],
-          title: 'Summary',
-          description: 'Provide a short overview of this recipe.',
-        },
-      },
-      required: [
-        'nid',
-        'uuid',
-        'vid',
-        'title',
-        'revision_translation_affected',
-        'path',
-        'field_difficulty',
-        'field_number_of_servings',
-        'field_preparation_time',
-        'field_recipe_instruction',
-        'field_summary',
-      ],
-    },
-    relationships: {
-      description: 'Entity relationships',
-      properties: {
-        type: {
-          type: 'object',
-          properties: {
-            data: {
-              type: 'object',
-              required: ['type', 'id'],
-              properties: {
-                type: {
-                  type: 'string',
-                  title: 'Referenced resource',
-                  enum: ['node_type--node_type'],
-                },
-                id: {
-                  type: 'string',
-                  title: 'Resource ID',
-                  format: 'uuid',
-                  maxLength: 128,
-                },
-              },
-            },
-          },
-          title: 'Resource Identifier',
-        },
-        revision_uid: {
-          type: 'object',
-          properties: {
-            data: {
-              type: 'object',
-              required: ['type', 'id'],
-              properties: {
-                type: {
-                  type: 'string',
-                  title: 'Referenced resource',
-                  enum: ['user--user'],
-                },
-                id: {
-                  type: 'string',
-                  title: 'Resource ID',
-                  format: 'uuid',
-                  maxLength: 128,
-                },
-              },
-            },
-          },
-          title: 'Resource Identifier',
-        },
-        uid: {
-          type: 'object',
-          properties: {
-            data: {
-              type: 'object',
-              required: ['type', 'id'],
-              properties: {
-                type: {
-                  type: 'string',
-                  title: 'Referenced resource',
-                  enum: ['user--user'],
-                },
-                id: {
-                  type: 'string',
-                  title: 'Resource ID',
-                  format: 'uuid',
-                  maxLength: 128,
-                },
-              },
-            },
-          },
-          title: 'Resource Identifier',
-        },
-        field_author: {
-          type: 'object',
-          properties: {
-            data: {
-              type: 'object',
-              required: ['type', 'id'],
-              properties: {
-                type: {
-                  type: 'string',
-                  title: 'Referenced resource',
-                  enum: ['user--user'],
-                },
-                id: {
-                  type: 'string',
-                  title: 'Resource ID',
-                  format: 'uuid',
-                  maxLength: 128,
-                },
-              },
-            },
-          },
-          title: 'Resource Identifier',
-        },
-        field_image: {
-          type: 'object',
-          properties: {
-            data: {
-              type: 'object',
-              required: ['type', 'id'],
-              properties: {
-                type: {
-                  type: 'string',
-                  title: 'Referenced resource',
-                  enum: ['file--file'],
-                },
-                id: {
-                  type: 'string',
-                  title: 'Resource ID',
-                  format: 'uuid',
-                  maxLength: 128,
-                },
-              },
-            },
-          },
-          title: 'Resource Identifier',
-        },
-        field_recipe_category: {
-          type: 'object',
-          properties: {
-            data: {
-              type: 'array',
-              items: {
-                type: 'object',
-                required: ['type', 'id'],
-                properties: {
-                  type: {
-                    type: 'string',
-                    title: 'Referenced resource',
-                    enum: ['taxonomy_term--recipe_category'],
-                  },
-                  id: {
-                    type: 'string',
-                    title: 'Resource ID',
-                    format: 'uuid',
-                    maxLength: 128,
-                  },
-                },
-              },
-            },
-          },
-          title: 'Resource Identifier',
-        },
-        field_tags: {
-          type: 'object',
-          properties: {
-            data: {
-              type: 'array',
-              items: {
-                type: 'object',
-                required: ['type', 'id'],
-                properties: {
-                  type: {
-                    type: 'string',
-                    title: 'Referenced resource',
-                    enum: ['taxonomy_term--tags'],
-                  },
-                  id: {
-                    type: 'string',
-                    title: 'Resource ID',
-                    format: 'uuid',
-                    maxLength: 128,
-                  },
-                },
-              },
-            },
-          },
-          title: 'Resource Identifier',
-        },
-      },
-      type: 'object',
-    },
-    links: {
-      type: 'object',
-      description: 'Entity links',
-      properties: {
-        self: {
-          type: 'string',
-          format: 'uri',
-          description: 'The absolute link to this entity.',
-        },
-      },
-    },
-  },
-  title: 'node:recipe Schema',
-  description: 'Add a new recipe to the site.',
-};

src/reducers/content.js
@@ -1,8 +1,13 @@
-import { CONTENT_LOADED, CONTENT_DELETE } from '../actions/content';
+import {
+  CONTENT_LOADED,
+  CONTENT_DELETE,
+  SCHEMA_LOADED,
+} from '../actions/content';
 
 export const initialState = {
   contentList: [],
   links: {},
+  schema: {},
 };
 
 export default (state = initialState, action) => {
@@ -36,6 +41,13 @@ export default (state = initialState, action) => {
         contentList: state.contentList.filter(
           content => content.id !== action.payload.content.id,
         ),
+      };
+    }
+
+    case SCHEMA_LOADED: {
+      return {
+        ...state,
+        schema: action.payload.schema,
       };
     }
 

src/routes.js
@@ -6,7 +6,6 @@ import Roles from './components/05_pages/Roles';
 import Dblog from './components/05_pages/Reports/Dblog';
 import NodeForm from './components/05_pages/NodeForm';
 import UiMetadata from './components/05_pages/NodeForm/UiMetadata';
-import RecipeSchema from './components/05_pages/NodeForm/RecipeSchema';
 import widgets from './components/05_pages/NodeForm/Widgets';
 
 // @todo Share this with Drupal
@@ -20,7 +19,6 @@ const routes = {
     <NodeForm
       entityTypeId="node"
       bundle="recipe"
-      schema={RecipeSchema}
       uiMetadata={UiMetadata}
       widgets={widgets}
     />

src/utils/api/api.js
@@ -109,6 +109,10 @@ async function api(
       );
       break;
     }
+    case 'schema': {
+      url = '/openapi/jsonapi';
+      break;
+    }
     default:
       break;
   }

commit 753f944548815f300b492fcb81d63c91d7b63863

Fix schema fetching (#227)
Resolves a small regression introduced by https://github.com/jsdrupal/drupal-admin-ui/pull/222

src/components/05_pages/NodeForm/index.js
@@ -74,8 +74,8 @@ class NodeForm extends React.Component {
   };
 
   getSchemaInfo = (schema, fieldName) =>
-    this.props.schema.properties.attributes.properties[fieldName] ||
-    this.props.schema.properties.relationships.properties[fieldName];
+    schema.properties.data.properties.attributes.properties[fieldName] ||
+    schema.properties.data.properties.relationships.properties[fieldName];
 
   render() {
     return (