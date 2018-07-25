30188df6492fffe544b48f65cf40771fc6b3380d
Lauri Eskola <lauri.eskola@acquia.com>
2018-07-25 04:46:41 +0300

    Remove dblog and permissions pages since they are not supported at the moment

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,6 @@
 import React from 'react';
 import AddContent from './components/05_pages/AddContent';
 import Content from './components/05_pages/Content/Content';
-import Permissions from './components/05_pages/Permissions/Permissions';
-import Roles from './components/05_pages/Roles';
-import Dblog from './components/05_pages/Reports/Dblog';
 import NodeForm from './components/05_pages/NodeForm';
 import UiMetadata from './components/05_pages/NodeForm/UiMetadata';
 import widgets from './components/05_pages/NodeForm/Widgets';
@@ -11,9 +8,6 @@ import widgets from './components/05_pages/NodeForm/Widgets';
 // @todo Share this with Drupal
 const routes = {
   '/admin/content': Content,
-  '/admin/people/permissions/:role?': Permissions,
-  '/admin/people/roles': Roles,
-  '/admin/reports/dblog': Dblog,
   '/node/add': AddContent,
   '/node/add/recipe': () => (
     <NodeForm
