5ffeb9652efe7f753fef036c28013617eff7d846
Matthew Grill <matt.grill@acquia.com>
2018-07-27 12:54:01 -0700

    Hide react-axe behind a cli flag.

--- src/index.js
+++ src/index.js
@@ -3,7 +3,9 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-if (process.env.NODE_ENV !== 'production') {
+// Disable react-axe without an additional cli flag.
+// See README.md for more information.
+if (process.env.NODE_ENV !== 'production' && process.env.REACT_APP_AXE) {
   // eslint-disable-next-line import/no-extraneous-dependencies, global-require
   const axe = require('react-axe');
   axe(React, ReactDOM);
