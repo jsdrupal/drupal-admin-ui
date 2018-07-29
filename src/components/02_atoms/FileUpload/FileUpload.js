60b669a35179afd9ab44ad7fdaac337060f6c454
Vijay Dubb <VD39@users.noreply.github.com>
2018-07-29 23:13:28 +0200

    Various updates to file upload widget

--- src/components/02_atoms/FileUpload/FileUpload.js
+++ src/components/02_atoms/FileUpload/FileUpload.js
@@ -1,62 +1,155 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import styled from 'react-emotion';
+import Card from '@material-ui/core/Card';
+import Button from '@material-ui/core/Button';
+import CardContent from '@material-ui/core/CardContent';
+import CloudUploadIcon from '@material-ui/icons/CloudUpload';
 import api from './../../../utils/api/api';
 
 const Element = styled('div')`
-  border: ${props => (props.drag ? '2px dashed red' : '2px dashed grey')};
+  border: 2px dashed grey;
   border-radius: 3px;
-  height: 150px;
+  padding: 80px;
+  width: 100%;
+  position: relative;
 `;
 
-class FileUpload extends React.Component {
+const Text = styled('div')`
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  margin: 0;
+  text-align: center;
+
+  .icon {
+    margin-left: 10px;
+  }
+`;
+
+const Error = styled('div')`
+  color: red;
+
+  > ul {
+    > li {
+      margin-bottom: 10px;
+    }
+
+    p {
+      margin: 0;
+    }
+  }
+`;
+
+class FileUpload extends Component {
   static propTypes = {
     entityTypeId: PropTypes.string.isRequired,
     bundle: PropTypes.string.isRequired,
     fieldName: PropTypes.string.isRequired,
     onFileUpload: PropTypes.func.isRequired,
+    inputProps: PropTypes.shape({
+      multiple: PropTypes.bool,
+      file_extensions: PropTypes.string,
+      max_filesize: PropTypes.number,
+    }).isRequired,
   };
 
+  /**
+   * Initial state
+   */
   state = {
-    drag: false,
-    file: null,
+    total: 0,
+    files: [],
+    errors: [],
+    filesLength: 0,
   };
 
+  /**
+   * Will set the border of the element to red and
+   * drop effect on drag over.
+   * @param {Event} event
+   */
   onDragOver = event => {
     event.stopPropagation();
     event.preventDefault();
-    this.setState({
-      drag: true,
-    });
+    this.setBorder('red');
     event.dataTransfer.dropEffect = 'dragend';
   };
 
+  /**
+   * Resets the border on drag leave.
+   * @param {Event} event
+   */
   onDragLeave = event => {
     event.stopPropagation();
     event.preventDefault();
-    this.setState({
-      drag: false,
-    });
+    this.setBorder();
   };
 
+  /**
+   * Will read the file/s that are dropped in the drop area.
+   * @param {Event} event
+   */
   onDrop = event => {
     event.stopPropagation();
     event.preventDefault();
+    this.readFile(event.dataTransfer.files);
+  };
 
-    const { files } = event.dataTransfer;
+  /**
+   * Will call the click event on the input to open the file explorer
+   * to allow file/s to be selected.
+   */
+  onClick = () => {
+    this.input.click();
+  };
+
+  /**
+   * Sets the border of the element.
+   * @param {String} [color="grey"] Color of the border.
+   */
+  setBorder = (color = 'grey') => {
+    const { element } = this;
+    if (element) {
+      element.style.border = `2px dashed ${color}`;
+    }
+  };
+
+  /**
+   * Will get the selected file/s from the file explorer.
+   * @param {Event} event
+   */
+  getFiles = event => {
+    this.readFile(event.target.files);
+  };
+
+  /**
+   * Uploads the file to the server, updates the total and files state.
+   * @param {Object} file File to be uploaded.
+   */
+  uploadFile = file => {
     const reader = new window.FileReader();
 
     reader.onloadend = async ({ target: { readyState, result } }) => {
       if (readyState === window.FileReader.DONE) {
+        const {
+          resetState,
+          props: { entityTypeId, bundle, fieldName, onFileUpload },
+        } = this;
         const { buffer } = new Uint8Array(result);
-
         const token = await api('csrf_token');
+        // Replace file name, some reason any space doesn't work
+        // TODO: Find a way to fix this without changing the name
+        const fileName = file.name.replace(/[,#!$^&*;{}=\-+`~()[\] ]/g, '_');
+
+        // Upload the file to server
         const createdFile = await api('file:upload', {
           parameters: {
-            entityTypeId: this.props.entityTypeId,
-            bundle: this.props.bundle,
-            fieldName: this.props.fieldName,
-            fileName: files[0].name,
+            bundle,
+            fileName,
+            fieldName,
+            entityTypeId,
             body: buffer,
           },
           options: {
@@ -67,34 +160,187 @@ class FileUpload extends React.Component {
         });
 
         this.setState(
-          {
-            drag: false,
-            file: createdFile,
+          prevState => ({
+            total: prevState.total + 1,
+            files: [...prevState.files, createdFile],
+          }),
+          () => {
+            onFileUpload(this.state.files);
+            resetState();
           },
-          () => this.props.onFileUpload(createdFile),
         );
       }
     };
 
-    // @TODO Handle multipe file uploads
-    reader.readAsArrayBuffer(files[0]);
+    reader.readAsArrayBuffer(file);
   };
 
-  render() {
+  /**
+   * Checks the current file has met all the criteria before
+   * being uploaded to the server. If not then will set the
+   * error state and update the total.
+   * Will check file size and extension.
+   * @param {String} type file type.
+   * @param {Number} size file size.
+   * @param {String} name file name.
+   */
+  checkFile = ({ type, size, name }) => {
+    /* eslint-disable camelcase */
+    const errors = {};
+    const extension = type.split('/')[1]; // <MIME_subtype>
+    const {
+      resetState,
+      props: {
+        inputProps: { max_filesize, file_extensions },
+      },
+    } = this;
+
+    // Check file size
+    if (size > max_filesize) {
+      // TODO: Convert max_filesize to MB
+      errors.size =
+        'The file could not be saved because it exceeds 2 MB, the maximum allowed size for uploads.';
+    }
+
+    // Check file extension
+    if (!file_extensions.includes(extension)) {
+      errors.type = `The image file is invalid or the image type is not allowed. Allowed types: ${file_extensions}.`;
+    }
+
+    // Check if there are errors
+    if (Object.keys(errors).length > 0) {
+      errors.name = `The specified file ${name} could not be uploaded.`;
+
+      // Set the state with error and update total
+      this.setState(
+        prevState => ({
+          total: prevState.total + 1,
+          errors: [...prevState.errors, errors],
+        }),
+        resetState,
+      );
+      return false;
+    }
+
+    return true;
+    /* eslint-enable camelcase */
+  };
+
+  /**
+   * Resets the state if all file/s have been uploaded.
+   */
+  resetState = () => {
+    const { total, filesLength } = this.state;
+    if (total === filesLength) {
+      this.setState({ files: [], filesLength: 0, total: 0 });
+    }
+  };
+
+  /**
+   * Will read the file/s, check if multiple files can be uploaded,
+   * check for errors, if no errors then upload the file.
+   * @param {Object} files Selected files.
+   */
+  readFile = files => {
+    const {
+      setBorder,
+      checkFile,
+      uploadFile,
+      props: {
+        inputProps: { multiple },
+      },
+    } = this;
+
+    this.setState({ filesLength: files.length, errors: [] });
+    setBorder();
+
+    if (multiple) {
+      Object.keys(files).forEach(key => {
+        const file = files[key];
+        if (checkFile(file)) {
+          uploadFile(file);
+        }
+      });
+    }
+
+    // If single file upload, upload the first file from the dropped files
+    if (!multiple && checkFile(files[0])) {
+      uploadFile(files[0]);
+    }
+  };
+
+  render = () => {
+    const {
+      onDrop,
+      onClick,
+      getFiles,
+      onDragOver,
+      onDragLeave,
+      state: { errors },
+      props: {
+        inputProps: { multiple },
+      },
+    } = this;
+
     return (
-      <Element
-        drag={this.state.drag}
-        onDragOver={this.onDragOver}
-        onDragLeave={this.onDragLeave}
-        onDrop={this.onDrop}
-      >
-        {(this.state.file &&
-          this.state.file.filename &&
-          this.state.file.filename[0].value) ||
-          'Drop a file or click here to upload one.'}
-      </Element>
+      <Card>
+        <CardContent>
+          <Element
+            onDrop={onDrop}
+            onClick={onClick}
+            onDragOver={onDragOver}
+            onDragLeave={onDragLeave}
+            innerRef={element => {
+              this.element = element;
+            }}
+          >
+            <Text>
+              <p>
+                {multiple
+                  ? 'Drop files or click here to upload.'
+                  : 'Drop a file or click here to upload.'}
+              </p>
+              <Button
+                size="small"
+                color="primary"
+                variant="contained"
+                aria-label="Upload Image/s"
+              >
+                Upload <CloudUploadIcon className="icon" />
+              </Button>
+            </Text>
+
+            <input
+              type="file"
+              onChange={getFiles}
+              multiple={multiple}
+              style={{ display: 'none' }}
+              ref={element => {
+                this.input = element;
+              }}
+            />
+          </Element>
+
+          {errors.length > 0 && (
+            <Error>
+              <p>One or more files could not be uploaded.</p>
+              <ul>
+                {errors.map(({ name, size, type }) => (
+                  <li>
+                    {name}
+                    <ul>
+                      {size && <li>{size}</li>}
+                      {type && <li>{type}</li>}
+                    </ul>
+                  </li>
+                ))}
+              </ul>
+            </Error>
+          )}
+        </CardContent>
+      </Card>
     );
-  }
+  };
 }
 
 export default FileUpload;
--- src/components/02_atoms/Widgets/FileUploadWidget.js
+++ src/components/02_atoms/Widgets/FileUploadWidget.js
@@ -1,72 +1,184 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import styled from 'react-emotion';
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
 import FormControl from '@material-ui/core/FormControl';
 import FormLabel from '@material-ui/core/FormLabel';
 import TextField from '@material-ui/core/TextField';
-import Card from '@material-ui/core/Card';
-import CardContent from '@material-ui/core/CardContent';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import Button from '@material-ui/core/Button';
+import DeleteIcon from '@material-ui/icons/Delete';
 
 import WidgetPropTypes from '../../05_pages/NodeForm/WidgetPropTypes';
 import FileUpload from '../FileUpload/FileUpload';
 
-const FileUploadWidget = props => (
-  <Card>
-    <CardContent>
-      <FormControl>
-        <FormLabel component="legend">{props.label}</FormLabel>
-        <FileUpload
-          bundle={props.bundle}
-          entityTypeId={props.entityTypeId}
-          fieldName={props.fieldName}
-          onFileUpload={file => {
-            props.onChange({
-              data: {
-                type: 'file--file',
-                id: file.uuid[0].value,
-              },
-            });
+const Element = styled('div')`
+  > div {
+    width: 100%;
+  }
+
+  legend {
+    margin-bottom: 10px;
+  }
+
+  .remove {
+    margin-left: auto;
+  }
+`;
+
+const Image = styled('div')`
+  > img {
+    max-width: 100px;
+    margin-right: 20px;
+  }
+`;
+
+const FileUploadWidget = ({
+  value,
+  label,
+  bundle,
+  onChange,
+  fieldName,
+  inputProps,
+  entityTypeId,
+}) => {
+  const length = value && value.data && Object.keys(value.data).length;
+  return (
+    <FormControl>
+      <Element>
+        <FormLabel component="legend">{label}</FormLabel>
+        <div
+          style={{
+            display: !inputProps.multiple && length ? 'none' : 'block',
           }}
-        />
-        {props.value && (
-          <TextField
-            value={(props.value.meta || {}).alt || ''}
-            onChange={event =>
-              props.onChange({
-                ...props.value,
-                meta: {
-                  ...(props.value.meta || {}),
-                  alt: event.target.value,
+        >
+          <FileUpload
+            bundle={bundle}
+            fieldName={fieldName}
+            inputProps={inputProps}
+            entityTypeId={entityTypeId}
+            onFileUpload={files => {
+              const data = files.reduce((arr, file) => {
+                arr[file.uuid[0].value] = {
+                  file: {
+                    type: 'file--file',
+                    url: file.url[0].value,
+                    id: file.uuid[0].value,
+                    filename: file.filename[0].value,
+                  },
+                  meta: { alt: '' },
+                };
+                return arr;
+              }, {});
+
+              onChange({
+                data: {
+                  ...value.data,
+                  ...data,
                 },
-              })
-            }
-            margin="normal"
-            label="Alternative text"
-            required
+              });
+            }}
           />
+        </div>
+
+        {length > 0 && (
+          <Card>
+            <CardContent>
+              <List>
+                {Object.keys(value.data).map(key => {
+                  const {
+                    meta: { alt },
+                    file: { url, filename },
+                  } = value.data[key];
+
+                  return (
+                    <ListItem key={key}>
+                      <Image>
+                        <img
+                          alt={alt || filename}
+                          src={`${process.env.REACT_APP_DRUPAL_BASE_URL}${url}`}
+                        />
+                      </Image>
+                      <TextField
+                        required
+                        value={alt}
+                        margin="normal"
+                        label="Alternative text"
+                        onChange={event =>
+                          onChange({
+                            data: {
+                              ...value.data,
+                              [key]: {
+                                ...value.data[key],
+                                meta: {
+                                  alt: event.target.value,
+                                },
+                              },
+                            },
+                          })
+                        }
+                      />
+                      <Button
+                        mini
+                        variant="fab"
+                        color="secondary"
+                        className="remove"
+                        aria-label="Remove Image"
+                        id={key}
+                        onClick={event => {
+                          const {
+                            data: { [event.currentTarget.id]: remove, ...data },
+                          } = value;
+                          onChange({
+                            data,
+                          });
+                        }}
+                      >
+                        <DeleteIcon />
+                      </Button>
+                    </ListItem>
+                  );
+                })}
+              </List>
+            </CardContent>
+          </Card>
         )}
-      </FormControl>
-    </CardContent>
-  </Card>
-);
+      </Element>
+    </FormControl>
+  );
+};
 
 FileUploadWidget.propTypes = {
   ...WidgetPropTypes,
   value: PropTypes.shape({
     data: PropTypes.shape({
-      uuid: PropTypes.arrayOf(
-        PropTypes.shape({
-          value: PropTypes.string.isRequired,
-        }),
-      ),
+      file: PropTypes.shape({
+        type: PropTypes.string.isRequired,
+        url: PropTypes.string.isRequired,
+        id: PropTypes.string.isRequired,
+        filename: PropTypes.string.isRequired,
+      }),
     }),
     meta: PropTypes.shape({
       alt: PropTypes.string,
     }),
   }),
+  inputProps: PropTypes.shape({
+    multiple: PropTypes.bool,
+    file_extensions: PropTypes.string,
+    max_filesize: PropTypes.number,
+  }),
 };
 
 FileUploadWidget.defaultProps = {
-  value: null,
+  value: {},
+  inputProps: {
+    multiple: true,
+    file_extensions: 'png gif jpg jpeg',
+    max_filesize: 2000000,
+  },
 };
 
 export default FileUploadWidget;
