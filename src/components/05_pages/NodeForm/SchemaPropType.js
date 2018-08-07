5bb629da0c8cf35ce0dc088e4fe857f2aba8d449
martin frances <martinfrances107@hotmail.com>
2018-08-07 20:05:34 +0100

    EntityReferenceAutocomplete schema is required #225. (#277)
    * EntityReferenceAutocomplete schema is required #225.
    
    * lint fixes

---
+++ src/components/05_pages/NodeForm/SchemaPropType.js
@@ -0,0 +1,14 @@
+import PropTypes from 'prop-types';
+
+const lazyFunction = f => (props, propName, componentName, ...rest) =>
+  f(props, propName, componentName, ...rest);
+
+let schemaType;
+const lazySchemaType = lazyFunction(() => schemaType);
+
+export default PropTypes.shape({
+  type: PropTypes.string.isRequired,
+  title: PropTypes.string,
+  description: PropTypes.string,
+  properties: PropTypes.objectOf(lazyFunction(lazySchemaType)),
+});
