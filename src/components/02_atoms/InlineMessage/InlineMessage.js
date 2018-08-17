0f242ff3d42a1585a5445bb3743c9fcf038f4f9c
Lauri Eskola <lauri.eskola@acquia.com>
2018-08-17 13:20:37 +0300

    Add warning and info to be supported in inline messages

--- src/components/02_atoms/InlineMessage/InlineMessage.js
+++ src/components/02_atoms/InlineMessage/InlineMessage.js
@@ -4,6 +4,8 @@ import PropTypes from 'prop-types';
 import {
   MESSAGE_SEVERITY_ERROR,
   MESSAGE_SEVERITY_SUCCESS,
+  MESSAGE_SEVERITY_INFO,
+  MESSAGE_SEVERITY_WARNING,
 } from '../../../constants/messages';
 
 let styles;
@@ -20,6 +22,8 @@ Message.propTypes = {
   messageSeverity: PropTypes.oneOf([
     MESSAGE_SEVERITY_ERROR,
     MESSAGE_SEVERITY_SUCCESS,
+    MESSAGE_SEVERITY_INFO,
+    MESSAGE_SEVERITY_WARNING,
   ]).isRequired,
 };
 
@@ -29,16 +33,24 @@ styles = {
     margin-bottom: 30px;
   `,
   success: css`
-    background-color: #d4edda;
+    background-color: #43a047;
   `,
   error: css`
-    background-color: #f8d7da;
+    background-color: #d32f2f;
+  `,
+  info: css`
+    background-color: #1976d2;
+  `,
+  warning: css`
+    background-color: #ffa000;
   `,
 };
 
 modifierClassMap = {
   [MESSAGE_SEVERITY_ERROR]: styles.error,
   [MESSAGE_SEVERITY_SUCCESS]: styles.success,
+  [MESSAGE_SEVERITY_WARNING]: styles.warning,
+  [MESSAGE_SEVERITY_INFO]: styles.info,
 };
 
 export default Message;
