eea94bbb16daab4a1fd403ff7d4bac83d5f072da
Lauri Eskola <lauri.eskola@acquia.com>
2018-08-23 11:47:16 +0300

    Move content table to its own component

--- src/components/05_pages/Content/Content.js
+++ src/components/05_pages/Content/Content.js
@@ -7,7 +7,6 @@ import { css } from 'emotion';
 import LoadingBar from 'react-redux-loading-bar';
 
 import Paper from '@material-ui/core/Paper';
-import Typography from '@material-ui/core/Typography';
 import Input from '@material-ui/core/Input';
 import InputLabel from '@material-ui/core/InputLabel';
 import MenuItem from '@material-ui/core/MenuItem';
@@ -18,22 +17,11 @@ import Checkbox from '@material-ui/core/Checkbox';
 import Chip from '@material-ui/core/Chip';
 import TextField from '@material-ui/core/TextField';
 
-import Table from '@material-ui/core/Table';
-import TableBody from '@material-ui/core/TableBody';
-import TableCell from '@material-ui/core/TableCell';
-import TableHead from '@material-ui/core/TableHead';
-import TableRow from '@material-ui/core/TableRow';
-import TableSortLabel from '@material-ui/core/TableSortLabel';
-import TablePagination from '@material-ui/core/TablePagination';
-
 import Button from '@material-ui/core/Button';
-import IconButton from '@material-ui/core/IconButton';
 import AddIcon from '@material-ui/icons/Add';
-import EditIcon from '@material-ui/icons/Edit';
-import DeleteIcon from '@material-ui/icons/Delete';
 
-import OpsModalButton from '../../02_atoms/OpsModalButton/OpsModalButton';
 import PageTitle from '../../02_atoms/PageTitle';
+import ContentTable from '../../04_templates/ContentTable/ContentTable';
 
 import {
   requestContentTypes,
@@ -43,7 +31,6 @@ import {
   requestContent,
   SUPPORTED_ACTIONS,
   actionExecute,
-  contentDelete,
 } from '../../../actions/content';
 
 const styles = {
@@ -121,7 +108,6 @@ class Content extends Component {
   };
   state = {
     contentTypes: [],
-    scrollToTop: false,
     status: null,
     sort: { path: 'changed', direction: 'DESC' },
     page: {
@@ -135,17 +121,6 @@ class Content extends Component {
     this.props.requestContentTypes();
     this.props.requestContent(this.state);
     this.props.requestActions();
-  }
-  componentDidUpdate(prevProps) {
-    // Operations executed in the bottom of the page should scroll back to top
-    // when list content changes. For example, the pagination uses this.
-    if (
-      this.state.scrollToTop &&
-      JSON.stringify(prevProps.contentList) !==
-        JSON.stringify(this.props.contentList)
-    ) {
-      window.scrollTo(0, this.table.offsetTop);
-    }
   }
   executeAction = () => {
     const matchingAction = this.props.actions.filter(
@@ -169,7 +144,6 @@ class Content extends Component {
     this.setState(
       ({ page: { limit } }) => ({
         page: { offset: page * limit, limit },
-        scrollToTop: true,
       }),
       () => {
         this.props.requestContent(this.state);
@@ -178,14 +152,6 @@ class Content extends Component {
   };
 
   render = () => {
-    const {
-      page: { offset, limit },
-    } = this.state;
-    const { links, contentList } = this.props;
-
-    // Calculate the highest known count.
-    const count = offset + contentList.length + (links.next ? 1 : 0);
-
     return (
       <div className={styles.root}>
         <PageTitle>Content</PageTitle>
@@ -337,186 +303,20 @@ class Content extends Component {
                     <AddIcon />
                   </Button>
                 </div>
-
-                <div
-                  ref={node => {
-                    this.table = node;
+                <ContentTable
+                  pageChangeHandler={this.pageChangeHandler}
+                  tableSortHandler={this.tableSortHandler}
+                  links={this.props.links}
+                  contentList={this.props.contentList}
+                  page={this.state.page}
+                  includes={this.props.includes}
+                  sort={this.state.sort}
+                  contentTypes={this.props.contentTypes}
+                  checked={this.state.checked}
+                  setChecked={values => {
+                    this.setState({ checked: values });
                   }}
-                >
-                  <Table>
-                    <TableHead>
-                      <TableRow>
-                        {<TableCell padding="checkbox" />}
-                        {[
-                          { key: 'title', label: 'Title', sortable: true },
-                          {
-                            key: 'type',
-                            label: 'Content Type',
-                            sortable: true,
-                          },
-                          this.props.includes['user--user']
-                            ? {
-                                key: 'author',
-                                label: 'Author',
-                                sortable: false,
-                              }
-                            : undefined,
-                          { key: 'status', label: 'Status', sortable: true },
-                          { key: 'changed', label: 'Updated', sortable: true },
-                          {
-                            key: 'operations',
-                            label: 'Operations',
-                            sortable: false,
-                          },
-                        ]
-                          .filter(x => x)
-                          .map(
-                            ({ key, label, sortable }) =>
-                              sortable ? (
-                                <TableCell key={key}>
-                                  <TableSortLabel
-                                    direction={
-                                      this.state.sort.path === key
-                                        ? this.state.sort.direction.toLowerCase()
-                                        : undefined
-                                    }
-                                    active={this.state.sort.path === key}
-                                    onClick={this.tableSortHandler(
-                                      key,
-                                      (this.state.sort.path !== key &&
-                                        'DESC') ||
-                                        ((this.state.sort.direction ===
-                                          'DESC' &&
-                                          'ASC') ||
-                                          'DESC'),
-                                    )}
-                                  >
-                                    {label}
-                                  </TableSortLabel>
-                                </TableCell>
-                              ) : (
-                                <TableCell key={key}>{label}</TableCell>
-                              ),
-                          )}
-                      </TableRow>
-                    </TableHead>
-                    <TableBody>
-                      {this.props.contentList.map(node => {
-                        const {
-                          type,
-                          attributes: { changed, nid, status, title },
-                          relationships,
-                        } = node;
-                        const rowSelectId = `row-select-for-${String(nid)}`;
-                        return (
-                          <TableRow key={nid}>
-                            {
-                              <TableCell padding="checkbox">
-                                <Checkbox
-                                  id={rowSelectId}
-                                  value={String(nid)}
-                                  onChange={(event, checked) => {
-                                    this.setState(prevState => {
-                                      prevState.checked[nid] = checked;
-                                      return prevState;
-                                    });
-                                  }}
-                                  checked={this.state.checked[nid] || false}
-                                />
-                              </TableCell>
-                            }
-                            <TableCell>
-                              <Link to={`/node/${nid}`}>
-                                <label htmlFor={rowSelectId}>{title}</label>
-                              </Link>
-                            </TableCell>
-                            <TableCell>
-                              {this.props.contentTypes[type].name}
-                            </TableCell>
-                            {this.props.includes['user--user'] && (
-                              <TableCell>
-                                {this.props.includes['user--user'][
-                                  relationships.uid.data.id
-                                ] ? (
-                                  <Link
-                                    to={`/user/${
-                                      this.props.includes['user--user'][
-                                        relationships.uid.data.id
-                                      ].attributes.uid
-                                    }`}
-                                  >
-                                    {
-                                      this.props.includes['user--user'][
-                                        relationships.uid.data.id
-                                      ].attributes.name
-                                    }
-                                  </Link>
-                                ) : (
-                                  'Anonymous (not verified)'
-                                )}
-                              </TableCell>
-                            )}
-                            <TableCell>
-                              {(status && 'Published') || 'Unpublished'}
-                            </TableCell>
-                            <TableCell>
-                              {new Intl.DateTimeFormat(navigator.language, {
-                                weekday: 'long',
-                                year: 'numeric',
-                                month: 'long',
-                                day: 'numeric',
-                                hour: 'numeric',
-                                minute: 'numeric',
-                              }).format(new Date(changed * 1000))}
-                            </TableCell>
-                            <TableCell style={{ whiteSpace: 'nowrap' }}>
-                              <IconButton
-                                aria-label="edit"
-                                className={styles.button}
-                                component={Link}
-                                to={`/node/${nid}/edit`}
-                              >
-                                <EditIcon />
-                              </IconButton>
-                              <OpsModalButton
-                                aria-label="delete"
-                                className={styles.button}
-                                title={`Are you sure that you want to delete this content ${title}?`}
-                                text="This action cannot be undone."
-                                cancelText="Cancel"
-                                confirmText="Delete"
-                                enterAction={() => {
-                                  this.props.contentDelete(node);
-                                }}
-                              >
-                                <DeleteIcon />
-                              </OpsModalButton>
-                            </TableCell>
-                          </TableRow>
-                        );
-                      })}
-                    </TableBody>
-                  </Table>
-                  <TablePagination
-                    component="div"
-                    count={count}
-                    rowsPerPage={limit}
-                    page={offset / limit}
-                    onChangePage={this.pageChangeHandler}
-                    rowsPerPageOptions={[limit]}
-                    labelDisplayedRows={({ page }) => `Page: ${page + 1}`}
-                    nextIconButtonProps={{ 'aria-label': 'Next content page.' }}
-                    backIconButtonProps={{
-                      'aria-label': 'Previous content page.',
-                    }}
-                  />
-                  {!this.props.contentList.length && (
-                    <Typography className={styles.noContentMessage}>
-                      There is no content yet.{' '}
-                      {<Link to="/node/add">Add one</Link>}.
-                    </Typography>
-                  )}
-                </div>
+                />
               </Fragment>
             )}
         </Paper>
@@ -542,6 +342,5 @@ export default connect(
     requestContentTypes,
     requestContent,
     actionExecute,
-    contentDelete,
   },
 )(Content);
