643a2e426ade14ee15e56dffbeea0a662050f757
Matthew Grill <matt.grill@acquia.com>
2018-08-27 12:16:28 -0700

    Use Loadable Components

--- src/routes.js
+++ src/routes.js
@@ -1,12 +1,19 @@
 import React from 'react';
-import AddContent from './components/05_pages/AddContent';
-import Content from './components/05_pages/Content/Content';
-import Permissions from './components/05_pages/Permissions/Permissions';
-import Roles from './components/05_pages/Roles';
-import Dblog from './components/05_pages/Reports/Dblog';
-import NodeEditForm from './components/05_pages/NodeEditForm';
+import loadable from 'loadable-components';
+
 import widgets from './components/05_pages/NodeForm/Widgets';
-import NodeAddForm from './components/05_pages/NodeAddForm';
+
+const AddContent = loadable(() => import('./components/05_pages/AddContent'));
+const Content = loadable(() => import('./components/05_pages/Content/Content'));
+const Permissions = loadable(() =>
+  import('./components/05_pages/Permissions/Permissions'),
+);
+const Roles = loadable(() => import('./components/05_pages/Roles'));
+const Dblog = loadable(() => import('./components/05_pages/Reports/Dblog'));
+const NodeEditForm = loadable(() =>
+  import('./components/05_pages/NodeEditForm'),
+);
+const NodeAddForm = loadable(() => import('./components/05_pages/NodeAddForm'));
 
 // @todo Share this with Drupal
 const routes = {
