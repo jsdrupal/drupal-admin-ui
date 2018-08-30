ec40db3d80d8603ffdd97e4910f5e8b221731784
Vishal Chandna <chandna.vishal1234@gmail.com>
2018-08-30 11:37:03 +0530

    Documentation added for the values object validation.

--- src/components/02_atoms/MultipleFields/MultipleFields.js
+++ src/components/02_atoms/MultipleFields/MultipleFields.js
@@ -171,6 +171,9 @@ class MultipleFields extends Component {
       state: { newItemAdded },
       props: { label, value: values, component, onChange },
     } = this;
+
+    // values && values.length is to validate the object is not null and not an empty array, respectively
+    // the last `&& values` is to make sure after validation, it always return `values`  instead of `values.length`
     const usedValues = (values && values.length && values) || [
       this.createEmptyItem(),
     ];
