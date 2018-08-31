8df1ac1171de9cc286a0a8c3b28dd77cb0f1e867
Matthew Grill <matt.grill@acquia.com>
2018-08-31 13:15:22 -0700

    cleanup how routes are handled.

--- src/routes.js
+++ src/routes.js
@@ -1,11 +1,9 @@
-import React from 'react';
 import AddContent from './components/05_pages/AddContent';
 import Content from './components/05_pages/Content/Content';
 import Permissions from './components/05_pages/Permissions/Permissions';
 import Roles from './components/05_pages/Roles';
 import Dblog from './components/05_pages/Reports/Dblog';
 import NodeEditForm from './components/05_pages/NodeEditForm';
-import widgets from './components/05_pages/NodeForm/Widgets';
 import NodeAddForm from './components/05_pages/NodeAddForm';
 
 // @todo Share this with Drupal
@@ -15,22 +13,8 @@ const routes = {
   '/admin/people/roles': Roles,
   '/admin/reports/dblog': Dblog,
   '/node/add': AddContent,
-  // eslint-disable-next-line react/prop-types
-  '/node/:nid/edit': ({ match }) => (
-    <NodeEditForm
-      entityTypeId="node"
-      widgets={widgets}
-      nid={match.params.nid}
-    />
-  ),
-  // eslint-disable-next-line react/prop-types
-  '/node/add/:bundle': ({ match }) => (
-    <NodeAddForm
-      entityTypeId="node"
-      bundle={match.params.bundle}
-      widgets={widgets}
-    />
-  ),
+  '/node/:nid/edit': NodeEditForm,
+  '/node/add/:bundle': NodeAddForm,
 };
 
 export default routes;
