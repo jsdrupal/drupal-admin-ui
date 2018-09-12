1ece4bf904960837f7e81f0f04910103b56c29ca
Matthew Grill <matt.grill@acquia.com>
2018-09-12 11:14:50 +0200

    fixes lint

--- src/utils/api/errors.js
+++ src/utils/api/errors.js
@@ -27,18 +27,25 @@ class ApiError extends Error {
     try {
       switch (error.status) {
         case 403:
-          return `You don't have access: ${(await error.response.json()).message} Maybe you aren't logged in.`;
+          return `You don't have access: ${
+            (await error.response.json()).message
+          } Maybe you aren't logged in.`;
         case 404:
-          return `Some page is missing: ${(await error.response.json()).message}`;
+          return `Some page is missing: ${
+            (await error.response.json()).message
+          }`;
         case 400:
-          return `You posted some invalid data, contact the administration team: ${(await error.response.json()).message}`;
+          return `You posted some invalid data, contact the administration team: ${
+            (await error.response.json()).message
+          }`;
         case 500:
-          return `The server crashed, contact the administration team: ${(await error.response.json()).message}`;
+          return `The server crashed, contact the administration team: ${
+            (await error.response.json()).message
+          }`;
         default:
           return error.toString();
       }
-    }
-    catch (e) {
+    } catch (e) {
       return error.toString();
     }
   }
