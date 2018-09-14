50ca7dc4df9b11b5b8f6d60ad5c26311169131ad
Jeevan Bhushetty <jeevan.bhushetty@qed42.com>
2018-09-14 15:59:01 +0530

    Adding unit test for messages

---
+++ src/components/02_atoms/InlineMessage/InlineMessage.test.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import InlineMessage from './InlineMessage';
+import 'jest-plugin-console-matchers/setup';
+
+describe('inline messages', () => {
+  it('severity error', () => {
+    const inlineMessage = 'This is sample message';
+    const severity = 'MESSAGE_SEVERITY_ERROR';
+    const message = shallow(
+      <InlineMessage message={inlineMessage} messageSeverity={severity} />,
+    );
+    expect(message.text()).toEqual('This is sample message');
+  });
+
+  it('without severity', () => {
+    expect(() => {
+      const inlineMessage = 'This is sample message';
+      const message = shallow(<InlineMessage message={inlineMessage} />);
+      return message.text();
+    }).toThrowWarning();
+  });
+});
