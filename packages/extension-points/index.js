1febe2ab67533ab320eaecba053b49a9e8a7eddc
Matthew Grill <matt.grill@acquia.com>
2018-11-05 15:49:37 -0800

    slightly improved take

--- packages/extension-points/index.js
+++ packages/extension-points/index.js
@@ -1,66 +1,90 @@
 #!/usr/bin/env node
-
-// Set NodeCofig directory
-process.env.NODE_CONFIG_DIR = `${process.cwd()}/jsDrupalConfig/`;
-
+const { promisify } = require('util');
+const { readFile } = require('fs');
+const { basename, resolve } = require('path');
+const { mode } = require('minimist')(process.argv.slice(2));
+const jsyaml = require('js-yaml');
 const webpack = require('webpack');
-const path = require('path');
 const MinifyPlugin = require('babel-minify-webpack-plugin');
-const config = require('config');
 
-const webpackConfig = {
-  entry: Object.assign({}, config.get('entry')),
-  optimization: {
-    minimize: false,
-  },
-  output: {
-    libraryTarget: 'umd',
-    library: 'jsDrupal_[name]',
-    path: path.resolve(process.cwd(), 'js', 'build'),
-    filename: '[name].js',
-  },
-  plugins: [],
-  externals: ['react', '@material-ui/core', /@material-ui\/core\/*./],
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        exclude: /(node_modules|bower_components)/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            plugins: ['@babel/plugin-proposal-class-properties'],
-            presets: [
-              '@babel/preset-react',
-              [
-                '@babel/preset-env',
-                {
-                  targets: {
-                    browsers: ['last 2 versions'],
-                  },
-                },
-              ],
-            ],
-          },
+const [readFilePM] = [readFile].map(promisify);
+const operatingDirectory = process.cwd();
+
+Promise.all([
+  readFilePM(
+    `${operatingDirectory}/${basename(
+      operatingDirectory,
+    )}.admin_ui.components.yml`,
+  ),
+])
+  .then(([components]) => ({ components: jsyaml.safeLoad(components) }))
+  .then(({ components }) =>
+    Object.keys(components.widgets).reduce(
+      (acc, widgetName) => ({
+        ...acc,
+        ...{
+          [`${widgetName}.widget`]: `./js/src/${basename(
+            components.widgets[widgetName].component,
+          )}`,
         },
+      }),
+      {},
+    ),
+  )
+  .then(entry => {
+    const webpackConfig = {
+      entry,
+      mode,
+      optimization: {
+        minimize: false,
       },
-    ],
-  },
-};
-
-if (config.has('mode') && config.get('mode') === 'production') {
-  webpackConfig.plugins.push(new MinifyPlugin());
-}
-
-webpack(webpackConfig).run((err, stats) => {
-  if (err) {
-    console.error(err);
-    return;
-  }
-  console.log(
-    stats.toString({
-      chunks: false,
-      colors: true,
-    }),
-  );
-});
+      output: {
+        libraryTarget: 'umd',
+        library: 'jsDrupal_[name]',
+        path: resolve(operatingDirectory, 'js', 'build'),
+        filename: '[name].js',
+      },
+      plugins: [],
+      externals: ['react', '@material-ui/core', /@material-ui\/core\/*./],
+      module: {
+        rules: [
+          {
+            test: /\.js$/,
+            exclude: /(node_modules|bower_components)/,
+            use: {
+              loader: 'babel-loader',
+              options: {
+                plugins: ['@babel/plugin-proposal-class-properties'],
+                presets: [
+                  '@babel/preset-react',
+                  [
+                    '@babel/preset-env',
+                    {
+                      targets: {
+                        browsers: ['last 2 versions'],
+                      },
+                    },
+                  ],
+                ],
+              },
+            },
+          },
+        ],
+      },
+    };
+    if (mode === 'production') {
+      webpackConfig.plugins.push(new MinifyPlugin());
+    }
+    webpack(webpackConfig).run((err, stats) => {
+      if (err) {
+        console.error(err);
+        return;
+      }
+      console.log(
+        stats.toString({
+          chunks: false,
+          colors: true,
+        }),
+      );
+    });
+  });
