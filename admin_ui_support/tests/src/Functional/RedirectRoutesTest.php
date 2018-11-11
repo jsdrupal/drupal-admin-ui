0ad7b92393c8ccd53a43e9d925638967cf2b0961
Daniel Wehner <dawehner@googlemail.com>
2018-11-11 18:59:27 +0000

    Fix tests for node/add/page

--- admin_ui_support/tests/src/Functional/RedirectRoutesTest.php
+++ admin_ui_support/tests/src/Functional/RedirectRoutesTest.php
@@ -60,7 +60,7 @@ class RedirectRoutesTest extends BrowserTestBase {
     $route_provider = \Drupal::service('router.route_provider');
     foreach ($paths as $path) {
       $routes = $route_provider->getRoutesByPattern($path);
-      $this->assertEquals(1, $routes->count());
+      $this->assertGreaterThanOrEqual(1, $routes->count());
       foreach ($routes as $route) {
         $this->assertEquals('Drupal\admin_ui_support\Controller\DefaultController::getAppRoute', $route->getDefault('_controller'));
       }
