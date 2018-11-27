43bbb090e40c1587e4d0e561436ce0ffec13f777
Matthew Grill <matt.grill@acquia.com>
2018-11-27 13:31:14 -0800

    Initial commit for design components

---
+++ packages/components/src/Components/H2/index.js
@@ -0,0 +1,3 @@
+import React from 'react';
+
+export default props => <h2>{props.children}</h2>;
---
+++ packages/components/src/index.js
@@ -0,0 +1,4 @@
+import H1 from './Components/H1';
+import H2 from './Components/H2';
+
+export { H1, H2 };
