e33bef4ec10a7f0cd604905f4ef99ea061c28192
Sally Young <sally@justafish.co.uk>
2018-12-20 18:58:41 +0000

    Fixes nightwatch tests against the production build

--- packages/admin-ui/src/tests/Nightwatch/Tests/TaxonomyPage.js
+++ packages/admin-ui/src/tests/Nightwatch/Tests/TaxonomyPage.js
@@ -7,45 +7,45 @@ module.exports = {
       .waitForElementVisible('th');
 
     browser.getText('th:nth-child(1)', function coloumnName(text) {
-      this.assert.equal(text.value.toLowerCase(), 'vocabulary name');
+      this.assert.equal(text.value, 'Vocabulary name');
     });
 
     browser.getText('th:nth-child(2)', function coloumnName(text) {
-      this.assert.equal(text.value.toLowerCase(), 'description');
+      this.assert.equal(text.value, 'Description');
     });
 
     browser.getText('th:nth-child(3)', function coloumnName(text) {
-      this.assert.equal(text.value.toLowerCase(), 'operations');
+      this.assert.equal(text.value, 'Operations');
     });
 
     browser
-      .waitForElementVisible('div[class^="MuiSelect-select"]')
-      .click('div[class^="MuiSelect-select"]');
+      .waitForElementVisible('[data-nightwatch="taxonomy-operations"] > div')
+      .click('[data-nightwatch="taxonomy-operations"] > div');
 
     browser.waitForElementVisible('#menu-');
 
     browser.getText('#menu- ul li:nth-child(1)', function menuItem(text) {
-      this.assert.equal(text.value.toLowerCase(), 'list terms');
+      this.assert.equal(text.value, 'List Terms');
     });
 
     browser.getText('#menu- ul li:nth-child(2)', function menuItem(text) {
-      this.assert.equal(text.value.toLowerCase(), 'edit vocabulary');
+      this.assert.equal(text.value, 'Edit Vocabulary');
     });
 
     browser.getText('#menu- ul li:nth-child(3)', function menuItem(text) {
-      this.assert.equal(text.value.toLowerCase(), 'add terms');
+      this.assert.equal(text.value, 'Add Terms');
     });
 
     browser.getText('#menu- ul li:nth-child(4)', function menuItem(text) {
-      this.assert.equal(text.value.toLowerCase(), 'manage dields');
+      this.assert.equal(text.value, 'Manage Fields');
     });
 
     browser.getText('#menu- ul li:nth-child(5)', function menuItem(text) {
-      this.assert.equal(text.value.toLowerCase(), 'manage form display');
+      this.assert.equal(text.value, 'Manage Form Display');
     });
 
     browser.getText('#menu- ul li:nth-child(6)', function menuItem(text) {
-      this.assert.equal(text.value.toLowerCase(), 'manage display');
+      this.assert.equal(text.value, 'Manage Display');
     });
 
     browser.click('#menu- ul li:nth-child(1)');
