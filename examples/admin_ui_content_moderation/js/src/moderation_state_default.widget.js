8d98477b4158681a14d9b31ace31eae771a08e60
Daniel Wehner <dawehner@googlemail.com>
2018-12-20 22:39:29 +0000

    more work

--- examples/admin_ui_content_moderation/js/src/moderation_state_default.widget.js
+++ examples/admin_ui_content_moderation/js/src/moderation_state_default.widget.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import api from 'drupal-admin-ui/src/api';
+import { api } from '@drupal/admin-ui-utilities';
 
 class ModerationStateDefaultWidget extends React.Component {
   state = {
@@ -9,25 +9,29 @@ class ModerationStateDefaultWidget extends React.Component {
   };
 
   componentDidMount() {
-    api('workflow', {}, {
-      query: {
+    console.log(api);
+    api('entity', {
+      parameters: {
+        entityType: 'workflow',
+      },
+      queryString: {
         // @todo pull this from the schema.
         filter: { condition: { path: 'id', value: 'test' } },
       },
-    })
-      .then(data => {
-        this.setState({
-          loaded: true,
-          data,
-        })
+    }).then(data => {
+      this.setState({
+        loaded: true,
+        data,
       });
+    });
   }
 
   render() {
+    console.log(this.state.data);
     // Use the possible options from this.state.data to
     // use a select element.
+    return '123';
   }
-
 }
 
 export default ModerationStateDefaultWidget;
