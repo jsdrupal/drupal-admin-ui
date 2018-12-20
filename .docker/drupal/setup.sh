b5571e0a240476f3b110efa6819cbacc8c65bdd3
Sally Young <sally@justafish.co.uk>
2018-12-20 18:08:37 +0000

    More permissions fixes

--- .docker/drupal/setup.sh
+++ .docker/drupal/setup.sh
@@ -5,7 +5,7 @@ rm -f /root/.drupal-installation-complete
 # If settings.php doesn't exist, create it and install Drupal
 if [ ! -f /var/www/drupal/docroot/sites/default/settings.php ]; then
     echo "settings.php not detected - installing Drupal (this may take some time, wait for a one-time login link to appear!)"
-    rm -rf config composer.json composer.lock vendor docroot/.ht.router.php docroot/core docroot/modules docroot/sites/default/files docroot/sites/default/settings.php docroot/sites/default/services.yml
+    rm -rf reports config composer.json composer.lock vendor docroot/.ht.router.php docroot/core docroot/modules docroot/sites/simpletest docroot/sites/default/files docroot/sites/default/settings.php docroot/sites/default/services.yml
     cp /var/www/drupal/docroot/sites/default/default.settings.php /var/www/drupal/docroot/sites/default/settings.php
     echo "\$settings['trusted_host_patterns'] = [
   '^drupal\$',
@@ -15,6 +15,9 @@ if [ ! -f /var/www/drupal/docroot/sites/default/settings.php ]; then
     cp templates/composer.lock composer.lock
     cp templates/.ht.router.php docroot/.ht.router.php
     echo "\$config_directories['sync'] = '../config/sync';" >> /var/www/drupal/docroot/sites/default/settings.php
+    mkdir docroot/sites/default/files docroot/sites/simpletest reports
+    chown apache:apache docroot/sites/default/files docroot/sites/simpletest reports
+    chmod 777 docroot/sites/default/files
     composer config repositories.repo-name path "/var/www/admin_ui_support"
     composer require justafish/drupal-admin-ui-support:dev-master
     composer install
@@ -22,11 +25,8 @@ if [ ! -f /var/www/drupal/docroot/sites/default/settings.php ]; then
     drush site:install demo_umami -y --db-url=mysql://drupal:drupal@mysql:3306/drupal --sites-subdir=default
     drush en -y jsonapi admin_ui_support admin_ui_widget_example
     drush config:set -y system.logging error_level verbose
+    rm -rf docroot/sites/default/files/styles
 fi
-
-rm -rf docroot/sites/default/files/styles reports
-mkdir reports
-chown apache:apache reports
 
 echo "##############################################################################################
 # One time login URL                                                                         #
