319c97df6c34677d11f3d4977cbd4835d81e4218
Sally Young <sally@justafish.co.uk>
2019-01-08 16:51:32 -0800

    [#599] Replace Apache with nginx and run services as non-root, with the new users having the same UID/GID as the host user to ensure correct volume permissions

--- .docker/node/setup.sh
+++ .docker/node/setup.sh
@@ -1,16 +1,17 @@
 #!/bin/sh
 
-rm -f /root/.yarn-build-complete
+rm -f /var/www/.yarn-build-complete
 
-if [ ! -f /root/drupal-admin-ui/packages/admin-ui/.env.local ]; then
+if [ ! -f /var/www/drupal-admin-ui/packages/admin-ui/.env.local ]; then
     cp packages/admin-ui/.env packages/admin-ui/.env.local
     chmod 666 packages/admin-ui/.env.local
 fi
 
+sudo chown -R node:node /var/www/.cache
 yarn install
 yarn workspace admin-ui build
 
 # Create a file to indicate build has finished
-touch /root/.yarn-build-complete
+touch /var/www/.yarn-build-complete
 
 yarn workspace admin-ui start
