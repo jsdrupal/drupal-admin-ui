ce0656774e4bc3054876d84d9ee30fc40a4733a8
Sally Young <sally@justafish.co.uk>
2019-01-18 16:07:32 +0000

    [#599] Make sure there isn't a user in the container with the same uid as the host system

---
+++ .docker/moveUser.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+
+# If a user or group exists at the specified IDs, move them to something else.
+NEW_UID=${HOST_UID}
+NEW_GID=${HOST_GID}
+
+if grep -q ${NEW_UID} /etc/passwd
+then
+  # Get next available ID
+  NEW_UID=$((HOST_UID + 1))
+  while grep -q ${NEW_UID} /etc/passwd
+  do
+    NEW_UID=$((NEW_UID + 1))
+  done
+  USERNAME=$(grep ${HOST_UID} /etc/passwd | cut -f1 -d:)
+  usermod -u ${NEW_UID} ${CONTAINER_USERNAME}
+  find / -user ${HOST_UID} -exec chown -h ${CONTAINER_USERNAME} {} \;
+fi
+
+if grep -q ${HOST_GID} /etc/group
+then
+  # Get next available ID
+  NEW_GID=$((HOST_GID + 1))
+  while grep -q ${NEW_GID} /etc/group
+  do
+    NEW_GID=$((NEW_GID + 1))
+  done
+  GROUPNAME=$(grep ${HOST_GID} /etc/group | cut -f1 -d:)
+  groupmod -g ${NEW_GID} ${GROUPNAME}
+  find / -group ${HOST_GID} -exec chgrp -h ${GROUPNAME} {} \;
+fi
