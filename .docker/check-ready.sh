6088754b97e2d518a3e218aaf9368c0945fe2efc
Martin Frances <martinfrances107@hotmail.com>
2019-01-27 18:06:20 +0000

    fixed the check-ready script

--- .docker/check-ready.sh
+++ .docker/check-ready.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 set -e
-until docker exec -it drupal_admin_ui_drupal test -e /var/www/.drupal-installation-complete && docker exec -it drupal_admin_ui_node test -e /var/www/.yarn-build-complete
+until docker exec -it drupal_admin_ui_drupal test -e /var/www/.drupal-installation-complete && docker exec -it drupal_admin_ui_node test -e /var/www/drupal-admin-ui/.yarn-build-complete
 do
     sleep 20
 done
